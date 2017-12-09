f297d6c89a3363894bb6eff36b8a12a51318fec8
James Coglan <james@jcoglan.com>
2017-12-09 19:33:04 +0000

    Use HEAD to set the parent of the new commit
    In the first commit, the `Commit` class did not record the ID of its
    parent commit. This was unnecessary since there were no previous
    commits. However, once `.git/HEAD` exists, each newly created commit
    should read it and store its contents as the ID of its parent.
    
    Here we introduce a very basic implementation of this feature, and a new
    abstraction for managing references: the `Refs` class. This has two
    methods: one for reading the current HEAD value (returning `nil` if
    there is no HEAD) and one for updating the value of HEAD.

--- commit.rb
+++ commit.rb
@@ -1,7 +1,8 @@
 class Commit
   attr_accessor :oid
 
-  def initialize(tree, author, message)
+  def initialize(parent, tree, author, message)
+    @parent  = parent
     @tree    = tree
     @author  = author
     @message = message
@@ -12,13 +13,14 @@ class Commit
   end
 
   def to_s
-    lines = [
-      "tree #{ @tree }",
-      "author #{ @author }",
-      "committer #{ @author }",
-      "",
-      @message
-    ]
+    lines = []
+
+    lines.push("tree #{ @tree }")
+    lines.push("parent #{ @parent }") if @parent
+    lines.push("author #{ @author }")
+    lines.push("committer #{ @author }")
+    lines.push("")
+    lines.push(@message)
 
     lines.join("\n")
   end
