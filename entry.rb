9892057d0cb630b803cb421df533c761b25204d2
James Coglan <james@jcoglan.com>
2017-12-13 09:29:36 +0000

    Record the mode of executable files.
    So far, all tree entries have had their mode hard-coded to "100644", the
    mode of regular files. In order to store the fact a file is executable,
    we must store it using the mode "100755".
    
    Here we achieve that by calling `stat(2)` on each file when we store
    blobs into the database, and passing the stat into the `Entry` class.
    When we serialise a `Tree` object, rather than hard-coding the mode of
    each entry as `100644`, we ask the entry for its mode, which it chooses
    by using the file stat to check if the file is executable by the current
    user.

--- entry.rb
+++ entry.rb
@@ -1,8 +1,16 @@
 class Entry
   attr_reader :name, :oid
 
-  def initialize(name, oid)
+  REGULAR_MODE    = "100644"
+  EXECUTABLE_MODE = "100755"
+
+  def initialize(name, oid, stat)
     @name = name
     @oid  = oid
+    @stat = stat
+  end
+
+  def mode
+    @stat.executable? ? EXECUTABLE_MODE : REGULAR_MODE
   end
 end
