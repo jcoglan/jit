721ac946ceb325dbe3b436fa651202f7431596c3
James Coglan <james@jcoglan.com>
2018-01-04 11:02:14 +0000

    An `add` command for writing a single file to the index.
    Here we begin to build up the machinery necessary for the `add` command.
    In this first pass, it takes a single file path, writes that file's
    contents to the database, and stores the resulting ID and stat data in
    the index.
    
    The process overwrites whatever the index currently contains. We don't
    yet have code for reading the index and modifying it; we just build a
    new empty `Index` in memory, add a single entry to it, and write it to
    disk.
    
    However, the serialisation logic present here is largely complete: the
    `Index::Entry` class writes all the necessary stat information out,
    including the file mode for executables, the blob ID is recorded
    correctly, and the index is finished with a SHA-1 hash. Comparison of
    this implementation with Git shows that the resulting `.git/index`
    contents are byte-for-byte identical after adding a single file.
    
    The index is not yet used to build commits -- the `commit` command still
    reads directly from the working tree.

---
+++ lib/index.rb
@@ -0,0 +1,46 @@
+require "digest/sha1"
+
+require_relative "./index/entry"
+require_relative "./lockfile"
+
+class Index
+  HEADER_FORMAT = "a4N2"
+
+  def initialize(pathname)
+    @entries  = {}
+    @lockfile = Lockfile.new(pathname)
+  end
+
+  def add(pathname, oid, stat)
+    entry = Entry.create(pathname, oid, stat)
+    @entries[pathname.to_s] = entry
+  end
+
+  def write_updates
+    return false unless @lockfile.hold_for_update
+
+    begin_write
+    header = ["DIRC", 2, @entries.size].pack(HEADER_FORMAT)
+    write(header)
+    @entries.each { |key, entry| write(entry.to_s) }
+    finish_write
+
+    true
+  end
+
+  private
+
+  def begin_write
+    @digest = Digest::SHA1.new
+  end
+
+  def write(data)
+    @lockfile.write(data)
+    @digest.update(data)
+  end
+
+  def finish_write
+    @lockfile.write(@digest.digest)
+    @lockfile.commit
+  end
+end
