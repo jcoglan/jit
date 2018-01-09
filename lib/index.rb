f106d5259dfb9817ab8c800e54b058669300c744
James Coglan <james@jcoglan.com>
2018-01-09 19:02:53 +0000

    Discard index files whose directories collide with files
    If the index contains the following files:
    
        alice.txt
        nested/bob.txt
        nested/inner/claire.txt
    
    and a file named `nested` is added, then the entries `nested/bob.txt`
    and `nested/inner/claire.txt` should be removed, since the name `nested`
    now corresponds to a file, not a directory.
    
    By adding a new structure to `Index` that tracks all the entries that
    are downstream of a particular directory, we make it easy to identify
    entries that must be removed when one of their parent directories is
    replaced by a file.

--- lib/index.rb
+++ lib/index.rb
@@ -76,19 +76,43 @@ class Index
   def clear
     @entries = {}
     @keys    = SortedSet.new
+    @parents = Hash.new { |hash, key| hash[key] = Set.new }
     @changed = false
   end
 
   def discard_conflicts(entry)
+    entry.parent_directories.each { |parent| remove_entry(parent) }
+    remove_children(entry.path)
+  end
+
+  def remove_entry(pathname)
+    entry = @entries[pathname.to_s]
+    return unless entry
+
+    @keys.delete(entry.key)
+    @entries.delete(entry.key)
+
     entry.parent_directories.each do |dirname|
-      @keys.delete(dirname.to_s)
-      @entries.delete(dirname.to_s)
+      dir = dirname.to_s
+      @parents[dir].delete(entry.path)
+      @parents.delete(dir) if @parents[dir].empty?
     end
+  end
+
+  def remove_children(path)
+    return unless @parents.has_key?(path)
+
+    children = @parents[path].clone
+    children.each { |child| remove_entry(child) }
   end
 
   def store_entry(entry)
     @keys.add(entry.key)
     @entries[entry.key] = entry
+
+    entry.parent_directories.each do |dirname|
+      @parents[dirname.to_s].add(entry.path)
+    end
   end
 
   def open_index_file
