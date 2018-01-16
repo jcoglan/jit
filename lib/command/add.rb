f5afd034af7e0cd191d03d71c6bd417f7568b38f
James Coglan <james@jcoglan.com>
2018-01-16 20:42:37 +0000

    Extract command code into classes
    The code in `bin/jit` would benefit from being encapsulated in classes
    so we can execute it from a test suite. This commit makes the first step
    of this process by transplanting the `bin/jit` code verbatim from its
    current location into one class for each command: `Command::Init`,
    `Command::Add` and `Command::Commit`.
    
    The command classes contain exactly the same code as was previously in
    `bin/jit` and will require further changes to make it easier to test.
    Those changes will be done in separate commits so they can be
    distinguished from this change that just moves a lot of code around.

---
+++ lib/command/add.rb
@@ -0,0 +1,57 @@
+require "pathname"
+require_relative "../repository"
+
+module Command
+  class Add
+
+    def run
+      root_path = Pathname.new(Dir.getwd)
+      repo = Repository.new(root_path.join(".git"))
+
+      begin
+        repo.index.load_for_update
+      rescue Lockfile::LockDenied => error
+        $stderr.puts <<~ERROR
+          fatal: #{ error.message }
+
+          Another jit process seems to be running in this repository.
+          Please make sure all processes are terminated then try again.
+          If it still fails, a jit process may have crashed in this
+          repository earlier: remove the file manually to continue.
+        ERROR
+        exit 128
+      end
+
+      begin
+        paths = ARGV.flat_map do |path|
+          path = Pathname.new(File.expand_path(path))
+          repo.workspace.list_files(path)
+        end
+      rescue Workspace::MissingFile => error
+        $stderr.puts "fatal: #{ error.message }"
+        repo.index.release_lock
+        exit 128
+      end
+
+      begin
+        paths.each do |path|
+          data = repo.workspace.read_file(path)
+          stat = repo.workspace.stat_file(path)
+
+          blob = Database::Blob.new(data)
+          repo.database.store(blob)
+          repo.index.add(path, blob.oid, stat)
+        end
+      rescue Workspace::NoPermission => error
+        $stderr.puts "error: #{ error.message }"
+        $stderr.puts "fatal: adding files failed"
+        repo.index.release_lock
+        exit 128
+      end
+
+      repo.index.write_updates
+      exit 0
+    end
+
+  end
+end
