ae24a5e7f88bc66691d18492f35bdb49d68ddaeb
James Coglan <james@jcoglan.com>
2018-01-19 20:19:16 +0000

    Refactor the `Command::Add` class
    While adding error handling to the `add` command, we made it much more
    complicated and hard to follow. Now that the code is in a class, and
    covered by tests, we can refactor it into a better shape.
    
    The error-handling code has been split into separate methods so it
    doesn't clutter up the main body of the command. Then, the details of
    each step of the command -- expanding the input paths, and adding each
    to the index -- are also split into their own methods. The `run` method
    is now just a high-level description of the steps taken by the command.

--- lib/command/add.rb
+++ lib/command/add.rb
@@ -4,50 +4,61 @@ require_relative "./base"
 module Command
   class Add < Base
 
+    LOCKED_INDEX_MESSAGE = <<~MSG
+      Another jit process seems to be running in this repository.
+      Please make sure all processes are terminated then try again.
+      If it still fails, a jit process may have crashed in this
+      repository earlier: remove the file manually to continue.
+    MSG
+
     def run
-      begin
-        repo.index.load_for_update
-      rescue Lockfile::LockDenied => error
-        @stderr.puts <<~ERROR
-          fatal: #{ error.message }
-
-          Another jit process seems to be running in this repository.
-          Please make sure all processes are terminated then try again.
-          If it still fails, a jit process may have crashed in this
-          repository earlier: remove the file manually to continue.
-        ERROR
-        exit 128
-      end
-
-      begin
-        paths = @args.flat_map do |path|
-          path = expanded_pathname(path)
-          repo.workspace.list_files(path)
-        end
-      rescue Workspace::MissingFile => error
-        @stderr.puts "fatal: #{ error.message }"
-        repo.index.release_lock
-        exit 128
-      end
-
-      begin
-        paths.each do |path|
-          data = repo.workspace.read_file(path)
-          stat = repo.workspace.stat_file(path)
-
-          blob = Database::Blob.new(data)
-          repo.database.store(blob)
-          repo.index.add(path, blob.oid, stat)
-        end
-      rescue Workspace::NoPermission => error
-        @stderr.puts "error: #{ error.message }"
-        @stderr.puts "fatal: adding files failed"
-        repo.index.release_lock
-        exit 128
-      end
-
+      repo.index.load_for_update
+      expanded_paths.each { |path| add_to_index(path) }
       repo.index.write_updates
       exit 0
+    rescue Lockfile::LockDenied => error
+      handle_locked_index(error)
+    rescue Workspace::MissingFile => error
+      handle_missing_file(error)
+    rescue Workspace::NoPermission => error
+      handle_unreadable_file(error)
+    end
+
+    private
+
+    def expanded_paths
+      @args.flat_map do |path|
+        repo.workspace.list_files(expanded_pathname(path))
+      end
+    end
+
+    def add_to_index(path)
+      data = repo.workspace.read_file(path)
+      stat = repo.workspace.stat_file(path)
+
+      blob = Database::Blob.new(data)
+      repo.database.store(blob)
+      repo.index.add(path, blob.oid, stat)
+    end
+
+    def handle_locked_index(error)
+      @stderr.puts "fatal: #{ error.message }"
+      @stderr.puts
+      @stderr.puts LOCKED_INDEX_MESSAGE
+      exit 128
+    end
+
+    def handle_missing_file(error)
+      @stderr.puts "fatal: #{ error.message }"
+      repo.index.release_lock
+      exit 128
+    end
+
+    def handle_unreadable_file(error)
+      @stderr.puts "error: #{ error.message }"
+      @stderr.puts "fatal: adding files failed"
+      repo.index.release_lock
+      exit 128
     end
 
   end
