e33232fa4e5823b39906ca2f52ad65672bf6481d
James Coglan <james@jcoglan.com>
2018-01-22 09:49:23 +0000

    Don't list untracked directories that contain no trackable files
    A further refinement of the previous edge case: if a directory is
    untracked, it should only be listed if it contains any files that would
    be added to the index if we passed the directory to the `add` command.
    In the absence of features for ignoring files, this just means the
    directory must, directly or indirectly, contain at least one file.
    
    To handle this we use a new recursive procedure that, rather than trying
    to discover the whole contents of a directory, merely tries to find a
    single file in the directory and returns as soon as it does so. This
    adds a little work to confirming a directory should be listed, but still
    allows us to skip fully exploring it.

--- lib/command/status.rb
+++ lib/command/status.rb
@@ -22,10 +22,25 @@ module Command
       repo.workspace.list_dir(prefix).each do |path, stat|
         if repo.index.tracked?(path)
           scan_workspace(path) if stat.directory?
-        else
+        elsif trackable_file?(path, stat)
           path += File::SEPARATOR if stat.directory?
           @untracked.add(path)
         end
+      end
+    end
+
+    def trackable_file?(path, stat)
+      return false unless stat
+
+      return !repo.index.tracked?(path) if stat.file?
+      return false unless stat.directory?
+
+      items = repo.workspace.list_dir(path)
+      files = items.select { |_, item_stat| item_stat.file? }
+      dirs  = items.select { |_, item_stat| item_stat.directory? }
+
+      [files, dirs].any? do |list|
+        list.any? { |item_path, item_stat| trackable_file?(item_path, item_stat) }
       end
     end
 
