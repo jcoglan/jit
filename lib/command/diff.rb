82f7c117dacef0f9badcf20f60fdb46f3ff9f11e
James Coglan <james@jcoglan.com>
2018-02-12 21:33:40 +0000

    Display diff headers for files deleted from the workspace
    When a file has been removed from the workspace but is still in the
    index, `diff` should print it with various items set to null-values in
    the diff headers. In particular, its object ID is `0000000`, and its
    path for the diff is `/dev/null`.
    
    The method for generating this output is simpler than that for modified
    files, because the mode of deleted files will always differ from that
    in the index, so some conditions the are present in the modified-file
    path are not needed here.

--- lib/command/diff.rb
+++ lib/command/diff.rb
@@ -4,6 +4,9 @@ require_relative "./base"
 module Command
   class Diff < Base
 
+    NULL_OID  = "0" * 40
+    NULL_PATH = "/dev/null"
+
     def run
       repo.index.load
       @status = repo.status
@@ -11,6 +14,7 @@ module Command
       @status.workspace_changes.each do |path, state|
         case state
         when :modified then diff_file_modified(path)
+        when :deleted  then diff_file_deleted(path)
         end
       end
 
@@ -45,6 +49,22 @@ module Command
       puts oid_range
       puts "--- #{ a_path }"
       puts "+++ #{ b_path }"
+    end
+
+    def diff_file_deleted(path)
+      entry  = repo.index.entry_for_path(path)
+      a_oid  = entry.oid
+      a_mode = entry.mode.to_s(8)
+      a_path = Pathname.new("a").join(path)
+
+      b_oid  = NULL_OID
+      b_path = Pathname.new("b").join(path)
+
+      puts "diff --git #{ a_path } #{ b_path }"
+      puts "deleted file mode #{ a_mode }"
+      puts "index #{ short a_oid }..#{ short b_oid }"
+      puts "--- #{ a_path }"
+      puts "+++ #{ NULL_PATH }"
     end
 
     def short(oid)
