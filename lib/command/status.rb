4536d956a32f9b39aa6e1cf490df65d032b16df8
James Coglan <james@jcoglan.com>
2018-02-12 19:30:37 +0000

    Extract status logic into `Repository::Status`
    This sets us up to work on the `diff` command. In order to know which
    files we should calculate and display the diffs of, without reading
    every file in the tree, we need to know which files differ between the
    HEAD commit, the index, and the workspace. That is, we need exactly the
    information that the `status` command displays.
    
    Here we extract the business logic from `Command::Status` into
    `Repository::Status` so that it can be reused by other commands. The
    moved code is kept pretty much verbatim, and the only code left in
    `Command::Status` is that related to printing the output, and opening
    and writing the index.
    
    The index logic should remain in the top-level command code rather than
    moving into reused business logic, because it's a piece of global state.
    If the top-level command has opened the index for updates and a piece of
    business logic then attempts the same thing, the second call will fail.
    Therefore we leave top-level commands in charge of how the index is
    opened.

--- lib/command/status.rb
+++ lib/command/status.rb
@@ -1,8 +1,4 @@
-require "pathname"
-require "set"
-
 require_relative "./base"
-require_relative "../sorted_hash"
 
 module Command
   class Status < Base
@@ -22,19 +18,8 @@ module Command
     }
 
     def run
-      @stats             = {}
-      @changed           = SortedSet.new
-      @index_changes     = SortedHash.new
-      @workspace_changes = SortedHash.new
-      @untracked_files   = SortedSet.new
-
       repo.index.load_for_update
-
-      scan_workspace
-      load_head_tree
-      check_index_entries
-      collect_deleted_head_files
-
+      @status = repo.status
       repo.index.write_updates
 
       print_results
@@ -52,9 +37,9 @@ module Command
     end
 
     def print_long_format
-      print_changes("Changes to be committed", @index_changes, :green)
-      print_changes("Changes not staged for commit", @workspace_changes, :red)
-      print_changes("Untracked files", @untracked_files, :red)
+      print_changes("Changes to be committed", @status.index_changes, :green)
+      print_changes("Changes not staged for commit", @status.workspace_changes, :red)
+      print_changes("Untracked files", @status.untracked_files, :red)
 
       print_commit_status
     end
@@ -72,11 +57,11 @@ module Command
     end
 
     def print_commit_status
-      return if @index_changes.any?
+      return if @status.index_changes.any?
 
-      if @workspace_changes.any?
+      if @status.workspace_changes.any?
         puts "no changes added to commit"
-      elsif @untracked_files.any?
+      elsif @status.untracked_files.any?
         puts "nothing added to commit but untracked files present"
       else
         puts "nothing to commit, working tree clean"
@@ -84,127 +69,21 @@ module Command
     end
 
     def print_porcelain_format
-      @changed.each do |path|
+      @status.changed.each do |path|
         status = status_for(path)
         puts "#{ status } #{ path }"
       end
 
-      @untracked_files.each do |path|
+      @status.untracked_files.each do |path|
         puts "?? #{ path }"
       end
     end
 
     def status_for(path)
-      left  = SHORT_STATUS.fetch(@index_changes[path], " ")
-      right = SHORT_STATUS.fetch(@workspace_changes[path], " ")
+      left  = SHORT_STATUS.fetch(@status.index_changes[path], " ")
+      right = SHORT_STATUS.fetch(@status.workspace_changes[path], " ")
 
       left + right
-    end
-
-    def record_change(path, set, type)
-      @changed.add(path)
-      set[path] = type
-    end
-
-    def scan_workspace(prefix = nil)
-      repo.workspace.list_dir(prefix).each do |path, stat|
-        if repo.index.tracked?(path)
-          @stats[path] = stat if stat.file?
-          scan_workspace(path) if stat.directory?
-        elsif trackable_file?(path, stat)
-          path += File::SEPARATOR if stat.directory?
-          @untracked_files.add(path)
-        end
-      end
-    end
-
-    def trackable_file?(path, stat)
-      return false unless stat
-
-      return !repo.index.tracked?(path) if stat.file?
-      return false unless stat.directory?
-
-      items = repo.workspace.list_dir(path)
-      files = items.select { |_, item_stat| item_stat.file? }
-      dirs  = items.select { |_, item_stat| item_stat.directory? }
-
-      [files, dirs].any? do |list|
-        list.any? { |item_path, item_stat| trackable_file?(item_path, item_stat) }
-      end
-    end
-
-    def load_head_tree
-      @head_tree = {}
-
-      head_oid = repo.refs.read_head
-      return unless head_oid
-
-      commit = repo.database.load(head_oid)
-      read_tree(commit.tree)
-    end
-
-    def read_tree(tree_oid, pathname = Pathname.new(""))
-      tree = repo.database.load(tree_oid)
-
-      tree.entries.each do |name, entry|
-        path = pathname.join(name)
-        if entry.tree?
-          read_tree(entry.oid, path)
-        else
-          @head_tree[path.to_s] = entry
-        end
-      end
-    end
-
-    def check_index_entries
-      repo.index.each_entry do |entry|
-        check_index_against_workspace(entry)
-        check_index_against_head_tree(entry)
-      end
-    end
-
-    def check_index_against_workspace(entry)
-      stat = @stats[entry.path]
-
-      unless stat
-        return record_change(entry.path, @workspace_changes, :deleted)
-      end
-
-      unless entry.stat_match?(stat)
-        return record_change(entry.path, @workspace_changes, :modified)
-      end
-
-      return if entry.times_match?(stat)
-
-      data = repo.workspace.read_file(entry.path)
-      blob = Database::Blob.new(data)
-      oid  = repo.database.hash_object(blob)
-
-      if entry.oid == oid
-        repo.index.update_entry_stat(entry, stat)
-      else
-        record_change(entry.path, @workspace_changes, :modified)
-      end
-    end
-
-    def check_index_against_head_tree(entry)
-      item = @head_tree[entry.path]
-
-      if item
-        unless entry.mode == item.mode and entry.oid == item.oid
-          record_change(entry.path, @index_changes, :modified)
-        end
-      else
-        record_change(entry.path, @index_changes, :added)
-      end
-    end
-
-    def collect_deleted_head_files
-      @head_tree.each_key do |path|
-        unless repo.index.tracked_file?(path)
-          record_change(path, @index_changes, :deleted)
-        end
-      end
     end
 
   end
