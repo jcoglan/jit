6fbd9821d6ee068ea2dbde4bcfbacb6282a33933
James Coglan <james@jcoglan.com>
2018-02-13 19:49:14 +0000

    Extract and unify diff printing logic
    The existing `diff_file_modified` and `diff_file_deleted` methods have
    some structural similarities despite their apparent differences. In
    particular, the code for printing output can be extracted into a single
    method, `print_diff`, that takes two `Target` objects and prints the
    difference between them.
    
    The `diff_file_{modified,deleted}` methods then reduce to code that
    constructs two of these `Target` objects and passes them to
    `print_diff`.

--- lib/command/diff.rb
+++ lib/command/diff.rb
@@ -7,6 +7,12 @@ module Command
     NULL_OID  = "0" * 40
     NULL_PATH = "/dev/null"
 
+    Target = Struct.new(:path, :oid, :mode) do
+      def diff_path
+        mode ? path : NULL_PATH
+      end
+    end
+
     def run
       repo.index.load
       @status = repo.status
@@ -26,49 +32,62 @@ module Command
     def diff_file_modified(path)
       entry  = repo.index.entry_for_path(path)
       a_oid  = entry.oid
-      a_mode = entry.mode.to_s(8)
-      a_path = Pathname.new("a").join(path)
+      a_mode = entry.mode
 
       blob   = Database::Blob.new(repo.workspace.read_file(path))
       b_oid  = repo.database.hash_object(blob)
-      b_mode = Index::Entry.mode_for_stat(@status.stats[path]).to_s(8)
-      b_path = Pathname.new("b").join(path)
+      b_mode = Index::Entry.mode_for_stat(@status.stats[path])
 
-      puts "diff --git #{ a_path } #{ b_path }"
+      a = Target.new(path, a_oid, a_mode.to_s(8))
+      b = Target.new(path, b_oid, b_mode.to_s(8))
 
-      unless a_mode == b_mode
-        puts "old mode #{ a_mode }"
-        puts "new mode #{ b_mode }"
-      end
-
-      return if a_oid == b_oid
-
-      oid_range = "index #{ short a_oid }..#{ short b_oid }"
-      oid_range.concat(" #{ a_mode }") if a_mode == b_mode
-
-      puts oid_range
-      puts "--- #{ a_path }"
-      puts "+++ #{ b_path }"
+      print_diff(a, b)
     end
 
     def diff_file_deleted(path)
       entry  = repo.index.entry_for_path(path)
       a_oid  = entry.oid
-      a_mode = entry.mode.to_s(8)
-      a_path = Pathname.new("a").join(path)
+      a_mode = entry.mode
 
-      b_oid  = NULL_OID
-      b_path = Pathname.new("b").join(path)
+      a = Target.new(path, a_oid, a_mode.to_s(8))
+      b = Target.new(path, NULL_OID, nil)
 
-      puts "diff --git #{ a_path } #{ b_path }"
-      puts "deleted file mode #{ a_mode }"
-      puts "index #{ short a_oid }..#{ short b_oid }"
-      puts "--- #{ a_path }"
-      puts "+++ #{ NULL_PATH }"
+      print_diff(a, b)
     end
 
     def short(oid)
       repo.database.short_oid(oid)
+    end
+
+    def print_diff(a, b)
+      return if a.oid == b.oid and a.mode == b.mode
+
+      a.path = Pathname.new("a").join(a.path)
+      b.path = Pathname.new("b").join(b.path)
+
+      puts "diff --git #{ a.path } #{ b.path }"
+      print_diff_mode(a, b)
+      print_diff_content(a, b)
+    end
+
+    def print_diff_mode(a, b)
+      if b.mode == nil
+        puts "deleted file mode #{ a.mode }"
+      elsif a.mode != b.mode
+        puts "old mode #{ a.mode }"
+        puts "new mode #{ b.mode }"
+      end
+    end
+
+    def print_diff_content(a, b)
+      return if a.oid == b.oid
+
+      oid_range = "index #{ short a.oid }..#{ short b.oid }"
+      oid_range.concat(" #{ a.mode }") if a.mode == b.mode
+
+      puts oid_range
+      puts "--- #{ a.diff_path }"
+      puts "+++ #{ b.diff_path }"
     end
 
   end
