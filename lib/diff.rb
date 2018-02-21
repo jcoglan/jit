137e8af3d2233b9b49f6372579e314e82cf6ad35
James Coglan <james@jcoglan.com>
2018-02-21 19:10:07 +0000

    Add line number information to `Diff::Edit` objects
    In order to make it easier to calculate and display hunk boundaries, it
    will be useful to have the output of `Diff.diff` contain line number
    information so we don't need to reconstruct it by walking through the
    edits.
    
    To that end, we replace the existing `text` field of `Diff::Edit` with
    two fields: `a_line` and `b_line`, each of which point to `Diff::Line`
    objects that have a `number` and `text`. This makes it much easier to
    tell which line numbers in the output match, since edits with type
    `:eql` have both an `a_line` and a `b_line`.
    
    When `Diff.diff` calls `Diff.lines`, this helper method now wraps each
    line of the file in a `Line` object with a 1-indexed number.
    
    Some minor changes to `Diff::Myers` are needed to cope with these new
    structures, but nothing in `Command::Diff` needs to change since it only
    relies on the `Diff::Edit#to_s` method.

--- lib/diff.rb
+++ lib/diff.rb
@@ -7,14 +7,18 @@ module Diff
     :del => "-"
   }
 
-  Edit = Struct.new(:type, :text) do
+  Line = Struct.new(:number, :text)
+
+  Edit = Struct.new(:type, :a_line, :b_line) do
     def to_s
-      SYMBOLS.fetch(type) + text
+      line = a_line || b_line
+      SYMBOLS.fetch(type) + line.text
     end
   end
 
   def self.lines(document)
-    document.is_a?(String) ? document.lines : document
+    document = document.lines if document.is_a?(String)
+    document.map.with_index { |text, i| Line.new(i + 1, text) }
   end
 
   def self.diff(a, b)
