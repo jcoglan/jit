1bcf703685b6f70e273b6668cc50dd3ad9e0756a
James Coglan <james@jcoglan.com>
2018-03-07 19:13:28 +0000

    Create branches from the current HEAD
    This commit introduces the ability to create branches, which are
    represented as pointers to commits, just like `.git/HEAD`. They're
    stored by name in the file `.git/refs/heads/<name>`.
    
    According to comments in the Git source [1], a branch is invalid if:
    
        - any path component of it begins with ".", or
        - it has double dots "..", or
        - it has ASCII control characters, or
        - it has ":", "?", "[", "\", "^", "~", SP, or TAB anywhere, or
        - it has "*" anywhere unless REFNAME_REFSPEC_PATTERN is set, or
        - it ends with a "/", or
        - it ends with ".lock", or
        - it contains a "@{" portion
    
    We represent these rules in the `INVALID_NAME` regex.
    
    The implementation in the `Refs` class generalises the logic for reading
    and writing `.git/HEAD` to arbitrary references, searching the `.git`,
    `.git/refs`, and `.git/refs/heads` directories for the given name. This
    means the same file-locking technique that's been used for writing HEAD
    is also used for writing branch pointers.
    
    [1]: https://github.com/git/git/blob/v2.15.0/refs.c#L58-L70

--- lib/refs.rb
+++ lib/refs.rb
@@ -1,28 +1,80 @@
+require "fileutils"
 require_relative "./lockfile"
 
 class Refs
+  InvalidBranch = Class.new(StandardError)
+
+  INVALID_NAME = /
+      ^\.
+    | \/\.
+    | \.\.
+    | ^\/
+    | \/$
+    | \.lock$
+    | @\{
+    | [\x00-\x20*:?\[\\^~\x7f]
+    /x
+
+  HEAD = "HEAD"
+
   def initialize(pathname)
-    @pathname = pathname
+    @pathname   = pathname
+    @refs_path  = @pathname.join("refs")
+    @heads_path = @refs_path.join("heads")
   end
 
   def read_head
-    if File.exist?(head_path)
-      File.read(head_path).strip
-    end
+    read_ref_file(@pathname.join(HEAD))
   end
 
   def update_head(oid)
-    lockfile = Lockfile.new(head_path)
+    update_ref_file(@pathname.join(HEAD), oid)
+  end
+
+  def read_ref(name)
+    path = path_for_name(name)
+    path ? read_ref_file(path) : nil
+  end
+
+  def create_branch(branch_name)
+    path = @heads_path.join(branch_name)
+
+    if INVALID_NAME =~ branch_name
+      raise InvalidBranch, "'#{ branch_name }' is not a valid branch name."
+    end
+
+    if File.file?(path)
+      raise InvalidBranch, "A branch named '#{ branch_name }' already exists."
+    end
+
+    update_ref_file(path, read_head)
+  end
+
+  private
+
+  def path_for_name(name)
+    prefixes = [@pathname, @refs_path, @heads_path]
+    prefix   = prefixes.find { |path| File.file? path.join(name) }
+
+    prefix ? prefix.join(name) : nil
+  end
+
+  def read_ref_file(path)
+    File.read(path).strip
+  rescue Errno::ENOENT
+    nil
+  end
+
+  def update_ref_file(path, oid)
+    lockfile = Lockfile.new(path)
 
     lockfile.hold_for_update
     lockfile.write(oid)
     lockfile.write("\n")
     lockfile.commit
-  end
 
-  private
-
-  def head_path
-    @pathname.join("HEAD")
+  rescue Lockfile::MissingParent
+    FileUtils.mkdir_p(path.dirname)
+    retry
   end
 end
