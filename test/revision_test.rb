9ee4646602beaffe38c730af10acf0899205df9c
James Coglan <james@jcoglan.com>
2018-03-07 20:30:17 +0000

    Parse revision notation
    Often we'd like to create a new branch pointing at something other than
    the current HEAD. While strictly speaking it is possible to do this
    using commit IDs, it is far from convenient, particularly as we don't
    yet have a `log` command or any other tooling for viewing the commit
    history, other than by manually inspecting the object database.
    
    Git includes a small query language for selecting commits, which it
    calls revisions [1]. Here we implement a parser for a small subset of
    this notation:
    
    - The `^` operator selects the parent of a commit
    - The `~<n>` operator selects the nth ancestor, e.g. `~3` == `^^^`
    - `@` is an alias for HEAD
    - All other strings not matching `INVALID_NAME` are references
    
    We don't yet interpret these strings, we just parse them into a
    structure for recognition, which can then be further processed.
    
    [1]: https://git-scm.com/docs/gitrevisions

---
+++ test/revision_test.rb
@@ -0,0 +1,61 @@
+require "minitest/autorun"
+require "revision"
+
+describe Revision do
+  describe "parse" do
+    def assert_parse(expression, tree)
+      assert_equal tree, Revision.parse(expression)
+    end
+
+    it "parses HEAD" do
+      assert_parse "HEAD",
+        Revision::Ref.new("HEAD")
+    end
+
+    it "parses @" do
+      assert_parse "@",
+        Revision::Ref.new("HEAD")
+    end
+
+    it "parses a branch name" do
+      assert_parse "master",
+        Revision::Ref.new("master")
+    end
+
+    it "parses an object ID" do
+      assert_parse "3803cb6dc4ab0a852c6762394397dc44405b5ae4",
+        Revision::Ref.new("3803cb6dc4ab0a852c6762394397dc44405b5ae4")
+    end
+
+    it "parses a parent ref" do
+      assert_parse "HEAD^",
+        Revision::Parent.new(Revision::Ref.new("HEAD"))
+    end
+
+    it "parses a chain of parent refs" do
+      assert_parse "master^^^",
+        Revision::Parent.new(
+          Revision::Parent.new(
+            Revision::Parent.new(
+              Revision::Ref.new("master"))))
+    end
+
+    it "parses an ancestor ref" do
+      assert_parse "@~3",
+        Revision::Ancestor.new(
+          Revision::Ref.new("HEAD"),
+          3)
+    end
+
+    it "parses a chain of parents and ancestors" do
+      assert_parse "@~2^^~3",
+        Revision::Ancestor.new(
+          Revision::Parent.new(
+            Revision::Parent.new(
+              Revision::Ancestor.new(
+                Revision::Ref.new("HEAD"),
+                2))),
+          3)
+    end
+  end
+end
