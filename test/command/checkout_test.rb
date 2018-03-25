3f194f81f24c614fec5718a7565d8f0e6f5666f9
James Coglan <james@jcoglan.com>
2018-03-25 16:23:23 +0100

    Detect conflicts during checkout
    Given the `Repository::Inspector` class extracted in the last commit,
    `Repository::Migration` can now check whether any of the files it plans
    to change contain uncommitted changes that would be lost if the checkout
    went ahead. Recall that a tree diff between HEAD and the target commit
    is a hash of the form:
    
        { Pathname => [Database::Entry, Database::Entry] }
    
    A path is considered conflicting with the tree diff if:
    
    - The current index entry for <path> does not match either of the
      entries in the tree diff on object ID and mode. Matching the first
      entry means the index is unchanged from HEAD, whereas matching the
      second means the index already contains the content from the target
      commit. Any other value means the index contains data that would be
      lost.
    
    - There is a file at <path> but it does not match the corresponding
      index entry, i.e. there are unstaged changes to the file.
    
    - There is a directory at <path> and it contains untracked files; these
      files would have to be removed to write the new content at <path>.
    
    - There is nothing at <path> but there is untracked content at a parent
      path. Again, this would have to be removed to accommodate the target
      commit's content.
    
    If any of these conditions hold, we record a conflict for the path. If
    there are any conflicts as a result of planning the changes, the
    migration does not go ahead and the conflicts are reported to the user.

--- test/command/checkout_test.rb
+++ test/command/checkout_test.rb
@@ -16,6 +16,42 @@ describe Command::Checkout do
       jit_cmd "checkout", revision
     end
 
+    def assert_stale_file(filename)
+      assert_stderr <<~ERROR
+        error: Your local changes to the following files would be overwritten by checkout:
+        \t#{ filename }
+        Please commit your changes or stash them before you switch branches.
+        Aborting
+      ERROR
+    end
+
+    def assert_stale_directory(filename)
+      assert_stderr <<~ERROR
+        error: Updating the following directories would lose untracked files in them:
+        \t#{ filename }
+
+        Aborting
+      ERROR
+    end
+
+    def assert_overwrite_conflict(filename)
+      assert_stderr <<~ERROR
+        error: The following untracked working tree files would be overwritten by checkout:
+        \t#{ filename }
+        Please move or remove them before you switch branches.
+        Aborting
+      ERROR
+    end
+
+    def assert_remove_conflict(filename)
+      assert_stderr <<~ERROR
+        error: The following untracked working tree files would be removed by checkout:
+        \t#{ filename }
+        Please move or remove them before you switch branches.
+        Aborting
+      ERROR
+    end
+
     def assert_status(status)
       jit_cmd "status", "--porcelain"
       assert_stdout status
@@ -41,6 +77,193 @@ describe Command::Checkout do
 
       assert_workspace base_files
       assert_status ""
+    end
+
+    it "fails to update a modified file" do
+      write_file "1.txt", "changed"
+      commit_all
+
+      write_file "1.txt", "conflict"
+
+      jit_cmd "checkout", "@^"
+      assert_stale_file "1.txt"
+    end
+
+    it "fails to update a modified-equal file" do
+      write_file "1.txt", "changed"
+      commit_all
+
+      write_file "1.txt", "1"
+
+      jit_cmd "checkout", "@^"
+      assert_stale_file "1.txt"
+    end
+
+    it "fails to update a changed-mode file" do
+      write_file "1.txt", "changed"
+      commit_all
+
+      make_executable "1.txt"
+
+      jit_cmd "checkout", "@^"
+      assert_stale_file "1.txt"
+    end
+
+    it "restores a deleted file" do
+      write_file "1.txt", "changed"
+      commit_all
+
+      delete "1.txt"
+      jit_cmd "checkout", "@^"
+
+      assert_workspace base_files
+      assert_status ""
+    end
+
+    it "restores files from a deleted directory" do
+      write_file "outer/inner/3.txt", "changed"
+      commit_all
+
+      delete "outer"
+      jit_cmd "checkout", "@^"
+
+      assert_workspace \
+        "1.txt"             => "1",
+        "outer/inner/3.txt" => "3"
+
+      assert_status <<~STATUS
+        \ D outer/2.txt
+      STATUS
+    end
+
+    it "fails to update a staged file" do
+      write_file "1.txt", "changed"
+      commit_all
+
+      write_file "1.txt", "conflict"
+      jit_cmd "add", "."
+
+      jit_cmd "checkout", "@^"
+      assert_stale_file "1.txt"
+    end
+
+    it "updates a staged-equal file" do
+      write_file "1.txt", "changed"
+      commit_all
+
+      write_file "1.txt", "1"
+      jit_cmd "add", "."
+      jit_cmd "checkout", "@^"
+
+      assert_workspace base_files
+      assert_status ""
+    end
+
+    it "fails to update a staged changed-mode file" do
+      write_file "1.txt", "changed"
+      commit_all
+
+      make_executable "1.txt"
+      jit_cmd "add", "."
+
+      jit_cmd "checkout", "@^"
+      assert_stale_file "1.txt"
+    end
+
+    it "fails to update an unindexed file" do
+      write_file "1.txt", "changed"
+      commit_all
+
+      delete "1.txt"
+      delete ".git/index"
+      jit_cmd "add", "."
+
+      jit_cmd "checkout", "@^"
+      assert_stale_file "1.txt"
+    end
+
+    it "fails to update an unindexed and untracked file" do
+      write_file "1.txt", "changed"
+      commit_all
+
+      delete "1.txt"
+      delete ".git/index"
+      jit_cmd "add", "."
+      write_file "1.txt", "conflict"
+
+      jit_cmd "checkout", "@^"
+      assert_stale_file "1.txt"
+    end
+
+    it "fails to update an unindexed directory" do
+      write_file "outer/inner/3.txt", "changed"
+      commit_all
+
+      delete "outer/inner"
+      delete ".git/index"
+      jit_cmd "add", "."
+
+      jit_cmd "checkout", "@^"
+      assert_stale_file "outer/inner/3.txt"
+    end
+
+    it "fails to update with a file at a parent path" do
+      write_file "outer/inner/3.txt", "changed"
+      commit_all
+
+      delete "outer/inner"
+      write_file "outer/inner", "conflict"
+
+      jit_cmd "checkout", "@^"
+      assert_stale_file "outer/inner/3.txt"
+    end
+
+    it "fails to update with a staged file at a parent path" do
+      write_file "outer/inner/3.txt", "changed"
+      commit_all
+
+      delete "outer/inner"
+      write_file "outer/inner", "conflict"
+      jit_cmd "add", "."
+
+      jit_cmd "checkout", "@^"
+      assert_stale_file "outer/inner/3.txt"
+    end
+
+    it "fails to update with an unstaged file at a parent path" do
+      write_file "outer/inner/3.txt", "changed"
+      commit_all
+
+      delete "outer/inner"
+      delete ".git/index"
+      jit_cmd "add", "."
+      write_file "outer/inner", "conflict"
+
+      jit_cmd "checkout", "@^"
+      assert_stale_file "outer/inner/3.txt"
+    end
+
+    it "fails to update with a file at a child path" do
+      write_file "outer/2.txt", "changed"
+      commit_all
+
+      delete "outer/2.txt"
+      write_file "outer/2.txt/extra.log", "conflict"
+
+      jit_cmd "checkout", "@^"
+      assert_stale_file "outer/2.txt"
+    end
+
+    it "fails to update with a staged file at a child path" do
+      write_file "outer/2.txt", "changed"
+      commit_all
+
+      delete "outer/2.txt"
+      write_file "outer/2.txt/extra.log", "conflict"
+      jit_cmd "add", "."
+
+      jit_cmd "checkout", "@^"
+      assert_stale_file "outer/2.txt"
     end
 
     it "removes a file" do
@@ -85,6 +308,187 @@ describe Command::Checkout do
       assert_status ""
     end
 
+    it "fails to remove a modified file" do
+      write_file "outer/94.txt", "94"
+      commit_all
+
+      write_file "outer/94.txt", "conflict"
+
+      jit_cmd "checkout", "@^"
+      assert_stale_file "outer/94.txt"
+    end
+
+    it "fails to remove a changed-mode file" do
+      write_file "outer/94.txt", "94"
+      commit_all
+
+      make_executable "outer/94.txt"
+
+      jit_cmd "checkout", "@^"
+      assert_stale_file "outer/94.txt"
+    end
+
+    it "leaves a deleted file deleted" do
+      write_file "outer/94.txt", "94"
+      commit_all
+
+      delete "outer/94.txt"
+      jit_cmd "checkout", "@^"
+
+      assert_workspace base_files
+      assert_status ""
+    end
+
+    it "leaves a deleted directory deleted" do
+      write_file "outer/inner/94.txt", "94"
+      commit_all
+
+      delete "outer/inner"
+      jit_cmd "checkout", "@^"
+
+      assert_workspace \
+        "1.txt"       => "1",
+        "outer/2.txt" => "2"
+
+      assert_status <<~STATUS
+        \ D outer/inner/3.txt
+      STATUS
+    end
+
+    it "fails to remove a staged file" do
+      write_file "outer/94.txt", "94"
+      commit_all
+
+      write_file "outer/94.txt", "conflict"
+      jit_cmd "add", "."
+
+      jit_cmd "checkout", "@^"
+      assert_stale_file "outer/94.txt"
+    end
+
+    it "fails to remove a staged changed-mode file" do
+      write_file "outer/94.txt", "94"
+      commit_all
+
+      make_executable "outer/94.txt"
+      jit_cmd "add", "."
+
+      jit_cmd "checkout", "@^"
+      assert_stale_file "outer/94.txt"
+    end
+
+    it "leaves an unindexed file deleted" do
+      write_file "outer/94.txt", "94"
+      commit_all
+
+      delete "outer/94.txt"
+      delete ".git/index"
+      jit_cmd "add", "."
+      jit_cmd "checkout", "@^"
+
+      assert_workspace base_files
+      assert_status ""
+    end
+
+    it "fails to remove an unindexed and untracked file" do
+      write_file "outer/94.txt", "94"
+      commit_all
+
+      delete "outer/94.txt"
+      delete ".git/index"
+      jit_cmd "add", "."
+      write_file "outer/94.txt", "conflict"
+
+      jit_cmd "checkout", "@^"
+      assert_remove_conflict "outer/94.txt"
+    end
+
+    it "leaves an unindexed directory deleted" do
+      write_file "outer/inner/94.txt", "94"
+      commit_all
+
+      delete "outer/inner"
+      delete ".git/index"
+      jit_cmd "add", "."
+      jit_cmd "checkout", "@^"
+
+      assert_workspace \
+        "1.txt"       => "1",
+        "outer/2.txt" => "2"
+
+      assert_status <<~STATUS
+        D  outer/inner/3.txt
+      STATUS
+    end
+
+    it "fails to remove with a file at a parent path" do
+      write_file "outer/inner/94.txt", "94"
+      commit_all
+
+      delete "outer/inner"
+      write_file "outer/inner", "conflict"
+
+      jit_cmd "checkout", "@^"
+      assert_stale_file "outer/inner/94.txt"
+    end
+
+    it "removes a file with a staged file at a parent path" do
+      write_file "outer/inner/94.txt", "94"
+      commit_all
+
+      delete "outer/inner"
+      write_file "outer/inner", "conflict"
+      jit_cmd "add", "."
+      jit_cmd "checkout", "@^"
+
+      assert_workspace \
+        "1.txt"       => "1",
+        "outer/2.txt" => "2",
+        "outer/inner" => "conflict"
+
+      assert_status <<~STATUS
+        A  outer/inner
+        D  outer/inner/3.txt
+      STATUS
+    end
+
+    it "fails to remove with an unstaged file at a parent path" do
+      write_file "outer/inner/94.txt", "94"
+      commit_all
+
+      delete "outer/inner"
+      delete ".git/index"
+      jit_cmd "add", "."
+      write_file "outer/inner", "conflict"
+
+      jit_cmd "checkout", "@^"
+      assert_remove_conflict "outer/inner"
+    end
+
+    it "fails to remove with a file at a child path" do
+      write_file "outer/94.txt", "94"
+      commit_all
+
+      delete "outer/94.txt"
+      write_file "outer/94.txt/extra.log", "conflict"
+
+      jit_cmd "checkout", "@^"
+      assert_stale_file "outer/94.txt"
+    end
+
+    it "removes a file with a staged file at a child path" do
+      write_file "outer/94.txt", "94"
+      commit_all
+
+      delete "outer/94.txt"
+      write_file "outer/94.txt/extra.log", "conflict"
+      jit_cmd "add", "."
+      jit_cmd "checkout", "@^"
+
+      assert_workspace base_files
+      assert_status ""
+    end
+
     it "adds a file" do
       delete "1.txt"
       commit_and_checkout "@^"
@@ -104,6 +508,85 @@ describe Command::Checkout do
     it "adds a directory" do
       delete "outer"
       commit_and_checkout "@^"
+
+      assert_workspace base_files
+      assert_status ""
+    end
+
+    it "fails to add an untracked file" do
+      delete "outer/2.txt"
+      commit_all
+
+      write_file "outer/2.txt", "conflict"
+
+      jit_cmd "checkout", "@^"
+      assert_overwrite_conflict "outer/2.txt"
+    end
+
+    it "fails to add an added file" do
+      delete "outer/2.txt"
+      commit_all
+
+      write_file "outer/2.txt", "conflict"
+      jit_cmd "add", "."
+
+      jit_cmd "checkout", "@^"
+      assert_stale_file "outer/2.txt"
+    end
+
+    it "adds a staged-equal file" do
+      delete "outer/2.txt"
+      commit_all
+
+      write_file "outer/2.txt", "2"
+      jit_cmd "add", "."
+      jit_cmd "checkout", "@^"
+
+      assert_workspace base_files
+      assert_status ""
+    end
+
+    it "fails to add with an untracked file at a parent path" do
+      delete "outer/inner/3.txt"
+      commit_all
+
+      delete "outer/inner"
+      write_file "outer/inner", "conflict"
+
+      jit_cmd "checkout", "@^"
+      assert_overwrite_conflict "outer/inner"
+    end
+
+    it "adds a file with an added file at a parent path" do
+      delete "outer/inner/3.txt"
+      commit_all
+
+      delete "outer/inner"
+      write_file "outer/inner", "conflict"
+      jit_cmd "add", "."
+      jit_cmd "checkout", "@^"
+
+      assert_workspace base_files
+      assert_status ""
+    end
+
+    it "fails to add with an untracked file at a child path" do
+      delete "outer/2.txt"
+      commit_all
+
+      write_file "outer/2.txt/extra.log", "conflict"
+
+      jit_cmd "checkout", "@^"
+      assert_stale_directory "outer/2.txt"
+    end
+
+    it "adds a file with an added file at a child path" do
+      delete "outer/2.txt"
+      commit_all
+
+      write_file "outer/2.txt/extra.log", "conflict"
+      jit_cmd "add", "."
+      jit_cmd "checkout", "@^"
 
       assert_workspace base_files
       assert_status ""
