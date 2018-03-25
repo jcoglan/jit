23707f5e171438392d5a156146d6054e5e68273d
James Coglan <james@jcoglan.com>
2018-03-25 16:23:23 +0100

    Detect conflicts during checkout
    Given the `Repository::Inspector` class extracted in the last commit,
    `Repository::Migration` can now check whether any of the files it plans
    to change contain uncommitted changes that would be lost if the checkout
    went ahead. Recall that a tree diff between HEAD and the target commit
    is a hash of the form:
    
        { Pathname => [Database::Entry, Database::Entry] }
    
    A path is considered conflicting with the tree diff if:
    
    - The current index entry for <path> does not match either of the
      entries in the tree diff on object ID and mode. Matching the first
      entry means the index is unchanged from HEAD, whereas matching the
      second means the index already contains the content from the target
      commit. Any other value means the index contains data that would be
      lost.
    
    - There is a file at <path> but it does not match the corresponding
      index entry, i.e. there are unstaged changes to the file.
    
    - There is a directory at <path> and it contains untracked files; these
      files would have to be removed to write the new content at <path>.
    
    - There is nothing at <path> but there is untracked content at a parent
      path. Again, this would have to be removed to accommodate the target
      commit's content.
    
    If any of these conditions hold, we record a conflict for the path. If
    there are any conflicts as a result of planning the changes, the
    migration does not go ahead and the conflicts are reported to the user.

--- lib/repository/migration.rb
+++ lib/repository/migration.rb
@@ -1,17 +1,49 @@
 require "set"
+require_relative "./inspector"
 
 class Repository
   class Migration
 
-    attr_reader :changes, :mkdirs, :rmdirs
+    Conflict = Class.new(StandardError)
+
+    MESSAGES = {
+      :stale_file => [
+        "Your local changes to the following files would be overwritten by checkout:",
+        "Please commit your changes or stash them before you switch branches."
+      ],
+      :stale_directory => [
+        "Updating the following directories would lose untracked files in them:",
+        "\n"
+      ],
+      :untracked_overwritten => [
+        "The following untracked working tree files would be overwritten by checkout:",
+        "Please move or remove them before you switch branches."
+      ],
+      :untracked_removed => [
+        "The following untracked working tree files would be removed by checkout:",
+        "Please move or remove them before you switch branches."
+      ]
+    }
+
+    attr_reader :changes, :mkdirs, :rmdirs, :errors
 
     def initialize(repository, tree_diff)
       @repo = repository
       @diff = tree_diff
 
+      @inspector = Inspector.new(repository)
+
       @changes = { :create => [], :update => [], :delete => [] }
       @mkdirs  = Set.new
       @rmdirs  = Set.new
+      @errors  = []
+
+      @conflicts = {
+        :stale_file            => SortedSet.new,
+        :stale_directory       => SortedSet.new,
+        :untracked_overwritten => SortedSet.new,
+        :untracked_removed     => SortedSet.new
+      }
     end
 
     def apply_changes
@@ -28,8 +60,11 @@ class Repository
 
     def plan_changes
       @diff.each do |path, (old_item, new_item)|
+        check_for_conflict(path, old_item, new_item)
         record_change(path, old_item, new_item)
       end
+
+      collect_errors
     end
 
     def update_workspace
@@ -61,6 +96,72 @@ class Repository
         action = :update
       end
       @changes[action].push([path, new_item])
+    end
+
+    def check_for_conflict(path, old_item, new_item)
+      entry = @repo.index.entry_for_path(path)
+
+      if index_differs_from_trees(entry, old_item, new_item)
+        @conflicts[:stale_file].add(path.to_s)
+        return
+      end
+
+      stat = @repo.workspace.stat_file(path)
+      type = get_error_type(stat, entry, new_item)
+
+      if stat == nil
+        parent = untracked_parent(path)
+        @conflicts[type].add(entry ? path.to_s : parent.to_s) if parent
+
+      elsif stat.file?
+        changed = @inspector.compare_index_to_workspace(entry, stat)
+        @conflicts[type].add(path.to_s) if changed
+
+      elsif stat.directory?
+        trackable = @inspector.trackable_file?(path, stat)
+        @conflicts[type].add(path.to_s) if trackable
+      end
+    end
+
+    def get_error_type(stat, entry, item)
+      if entry
+        :stale_file
+      elsif stat&.directory?
+        :stale_directory
+      elsif item
+        :untracked_overwritten
+      else
+        :untracked_removed
+      end
+    end
+
+    def index_differs_from_trees(entry, old_item, new_item)
+      @inspector.compare_tree_to_index(old_item, entry) and
+      @inspector.compare_tree_to_index(new_item, entry)
+    end
+
+    def untracked_parent(path)
+      path.dirname.ascend.find do |parent|
+        next if parent.to_s == "."
+
+        parent_stat = @repo.workspace.stat_file(parent)
+        next unless parent_stat&.file?
+
+        @inspector.trackable_file?(parent, parent_stat)
+      end
+    end
+
+    def collect_errors
+      @conflicts.each do |type, paths|
+        next if paths.empty?
+
+        lines = paths.map { |name| "\t#{ name }" }
+        header, footer = MESSAGES.fetch(type)
+
+        @errors.push([header, *lines, footer].join("\n"))
+      end
+
+      raise Conflict unless @errors.empty?
     end
 
   end
