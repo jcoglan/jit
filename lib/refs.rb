29a1365a6ef525127efc21b122f1dcb6a6dd6556
James Coglan <james@jcoglan.com>
2018-04-02 19:52:11 +0100

    Introduce symbolic references for checking out branches
    Say we have the following history, where HEAD (`.git/HEAD`) and `foo`
    (`.git/refs/heads/foo`) point at the latest commit, and `bar`
    (`.git/refs/heads/bar`) points at HEAD's grandparent.
    
        A       B       C       D
        o <---- o <---- o <---- o
                |               |
              [bar]           [foo]
                              [HEAD]
    
    `foo` and `bar` were created using the `branch` command. Currently, if
    we run `checkout bar`, the index and workspace are migrated and HEAD is
    updated so that it points at the same commit as `bar`.
    
        A       B       C       D
        o <---- o <---- o <---- o
                |               |
              [bar]           [foo]
              [HEAD]
    
    However, this doesn't result in the `bar` branch growing when we make
    new commits. The `commit` command updates HEAD to point to the new
    commit, but `bar` remains unchanged. HEAD and `bar` are not connected.
    
        A       B       C       D
        o <---- o <---- o <---- o
               / \              |
            [bar] \           [foo]
                   \
                    \ X
                     o
                     |
                   [HEAD]
    
    As we add more commits, HEAD drifts away from `bar`, whereas we'd rather
    that the `bar` branch pointer followed along so that we retain a
    reference to this line of history after we check out some other commit
    and HEAD no longer points at it. That is, instead of this:
    
        A       B       C       D
        o <---- o <---- o <---- o
               / \              |
            [bar] \           [foo]
                   \
                    \ X      Y       Z
                     o <---- o <---- o
                                     |
                                   [HEAD]
    
    We want this:
    
        A       B       C       D
        o <---- o <---- o <---- o
                 \              |
                  \           [foo]
                   \
                    \ X      Y       Z
                     o <---- o <---- o
                                     |
                                   [bar] <== [HEAD]
    
    That `<==` arrow represents a symbolic reference or symref. When we run
    `checkout bar`, instead of writing the new commit ID directly to
    `.git/HEAD`, we can instead write this:
    
        ref: refs/heads/bar
    
    HEAD is now a symref to `bar`, and any operation that wants to read or
    update HEAD should instead update whatever branch reference HEAD is
    pointing at.
    
    In this commit we introduce the `Refs#set_head` method, which takes an
    object ID and the original unresolved revision name that `checkout` was
    run with. If that name is the name of a branch, then `Refs` will write a
    symref to `.git/HEAD`, otherwise it writes the object ID as usual. We
    also update the `read_head` and `read_ref` methods so that they traverse
    symrefs, and introduce the `current_ref` method which returns the
    identify of the reference that HEAD currently points at.

--- lib/refs.rb
+++ lib/refs.rb
@@ -6,7 +6,11 @@ require_relative "./revision"
 class Refs
   InvalidBranch = Class.new(StandardError)
 
-  HEAD = "HEAD"
+  SymRef = Struct.new(:path)
+  Ref    = Struct.new(:oid)
+
+  HEAD   = "HEAD"
+  SYMREF = /^ref: (.+)$/
 
   def initialize(pathname)
     @pathname   = pathname
@@ -15,16 +19,28 @@ class Refs
   end
 
   def read_head
-    read_ref_file(@pathname.join(HEAD))
+    read_symref(@pathname.join(HEAD))
   end
 
   def update_head(oid)
     update_ref_file(@pathname.join(HEAD), oid)
   end
 
+  def set_head(revision, oid)
+    head = @pathname.join(HEAD)
+    path = @heads_path.join(revision)
+
+    if File.file?(path)
+      relative = path.relative_path_from(@pathname)
+      update_ref_file(head, "ref: #{ relative }")
+    else
+      update_ref_file(head, oid)
+    end
+  end
+
   def read_ref(name)
     path = path_for_name(name)
-    path ? read_ref_file(path) : nil
+    path ? read_symref(path) : nil
   end
 
   def create_branch(branch_name, start_oid)
@@ -41,6 +57,15 @@ class Refs
     update_ref_file(path, start_oid)
   end
 
+  def current_ref(source = HEAD)
+    ref = read_oid_or_symref(@pathname.join(source))
+
+    case ref
+    when SymRef   then current_ref(ref.path)
+    when Ref, nil then SymRef.new(source)
+    end
+  end
+
   private
 
   def path_for_name(name)
@@ -50,10 +75,22 @@ class Refs
     prefix ? prefix.join(name) : nil
   end
 
-  def read_ref_file(path)
-    File.read(path).strip
+  def read_oid_or_symref(path)
+    data  = File.read(path).strip
+    match = SYMREF.match(data)
+
+    match ? SymRef.new(match[1]) : Ref.new(data)
   rescue Errno::ENOENT
     nil
+  end
+
+  def read_symref(path)
+    ref = read_oid_or_symref(path)
+
+    case ref
+    when SymRef then read_symref(@pathname.join(ref.path))
+    when Ref    then ref.oid
+    end
   end
 
   def update_ref_file(path, oid)
