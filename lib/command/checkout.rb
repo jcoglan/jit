0dede851e083a24ae3e24a43635cf7f3c3cb1a58
James Coglan <james@jcoglan.com>
2018-04-02 20:22:20 +0100

    Print results on successful checkout
    When a checkout succeeds, we should tell the user what happened,
    informing them about the commit they just left and the one that's now
    checked out.
    
    If they had a branch checked out before the checkout, then they've not
    lost the reference to that commit and so we don't need to print
    anything. However if HEAD was a direct pointer to a commit, the checkout
    might have destroyed the only reference to it, so we print a message
    telling the user what it was, e.g.
    
        Previous HEAD position was 59dd0c5 Initial revision of "git"
    
    Similarly, if the user checked out a branch, we just print that branch's
    name, otherwise we print information about the new commit.
    
    If the user had a branch checked out (i.e. HEAD was a symref) before the
    checkout, but this is no longer the case, we also inform them about
    "detached HEAD" state. It's still possible to make commits, it's just
    that no branch will refer to them, so they may be lost if another branch
    is checked out.
    
    (The `commit` command does not yet move branch pointers when HEAD is a
    symref, but this will change shortly.)

--- lib/command/checkout.rb
+++ lib/command/checkout.rb
@@ -4,10 +4,22 @@ require_relative "../revision"
 module Command
   class Checkout < Base
 
+    DETACHED_HEAD_MESSAGE = <<~MSG
+      You are in 'detached HEAD' state. You can look around, make experimental
+      changes and commit them, and you can discard any commits you make in this
+      state without impacting any branches by performing another checkout.
+
+      If you want to create a new branch to retain commits you create, you may
+      do so (now or later) by using the branch command. Example:
+
+        jit branch <new-branch-name>
+    MSG
+
     def run
       @target = @args[0]
 
-      @current_oid = repo.refs.read_head
+      @current_ref = repo.refs.current_ref
+      @current_oid = @current_ref.read_oid
 
       revision    = Revision.new(repo, @target)
       @target_oid = revision.resolve(Revision::COMMIT)
@@ -20,6 +32,11 @@ module Command
 
       repo.index.write_updates
       repo.refs.set_head(@target, @target_oid)
+      @new_ref = repo.refs.current_ref
+
+      print_previous_head
+      print_detachment_notice
+      print_new_head
 
       exit 0
 
@@ -49,6 +66,38 @@ module Command
       end
       @stderr.puts "error: #{ error.message }"
       exit 1
+    end
+
+    def print_previous_head
+      if @current_ref.head? and @current_oid != @target_oid
+        print_head_position("Previous HEAD position was", @current_oid)
+      end
+    end
+
+    def print_detachment_notice
+      return unless @new_ref.head? and not @current_ref.head?
+
+      @stderr.puts "Note: checking out '#{ @target }'."
+      @stderr.puts ""
+      @stderr.puts DETACHED_HEAD_MESSAGE
+      @stderr.puts ""
+    end
+
+    def print_new_head
+      if @new_ref.head?
+        print_head_position("HEAD is now at", @target_oid)
+      elsif @new_ref == @current_ref
+        @stderr.puts "Already on '#{ @target }'"
+      else
+        @stderr.puts "Switched to branch '#{ @target }'"
+      end
+    end
+
+    def print_head_position(message, oid)
+      commit = repo.database.load(oid)
+      short  = repo.database.short_oid(commit.oid)
+
+      @stderr.puts "#{ message } #{ short } #{ commit.title_line }"
     end
 
   end
