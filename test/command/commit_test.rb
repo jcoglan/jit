ada1ba006bc8a71c89ebf4c4327e52c725fe3b6f
James Coglan <james@jcoglan.com>
2018-04-06 20:13:42 +0100

    Update branch pointers on commit
    To complete the utility of the `branch` and `checkout` commands, we need
    the `commit` command to make branch pointers track commit chains, rather
    than just moving HEAD around. That is, if `.git/HEAD` contains
    
        ref: ref/heads/foo
    
    then `commit` should write the new commit ID to `.git/refs/heads/foo`,
    rather than overwriting `.git/HEAD` and severing the link between HEAD
    and the branch.
    
    To enable this, we make `Refs#update_head` follow symrefs until it
    reaches a ref that either does not exist or contains an object ID. As
    this chain of refs is followed, each ref in turn is locked so that the
    ref does not change between the time it's read and the time its target
    is updated. Once the ultimate target ref is updated, all locks on refs
    leading to it are released.

---
+++ test/command/commit_test.rb
@@ -0,0 +1,91 @@
+require "minitest/autorun"
+require "command_helper"
+
+describe Command::Commit do
+  include CommandHelper
+
+  describe "committing to branches" do
+    before do
+      ["first", "second", "third"].each do |message|
+        write_file "file.txt", message
+        jit_cmd "add", "."
+        commit message
+      end
+
+      jit_cmd "branch", "topic"
+      jit_cmd "checkout", "topic"
+    end
+
+    def commit_change(content)
+      write_file "file.txt", content
+      jit_cmd "add", "."
+      commit content
+    end
+
+    describe "on a branch" do
+      it "advances a branch pointer" do
+        head_before = repo.refs.read_ref("HEAD")
+
+        commit_change "change"
+
+        head_after   = repo.refs.read_ref("HEAD")
+        branch_after = repo.refs.read_ref("topic")
+
+        refute_equal head_before, head_after
+        assert_equal head_after, branch_after
+
+        assert_equal head_before,
+                     resolve_revision("@^")
+      end
+    end
+
+    describe "with a detached HEAD" do
+      before do
+        jit_cmd "checkout", "@"
+      end
+
+      it "advances HEAD" do
+        head_before = repo.refs.read_ref("HEAD")
+        commit_change "change"
+        head_after = repo.refs.read_ref("HEAD")
+
+        refute_equal head_before, head_after
+      end
+
+      it "does not advance the detached branch" do
+        branch_before = repo.refs.read_ref("topic")
+        commit_change "change"
+        branch_after = repo.refs.read_ref("topic")
+
+        assert_equal branch_before, branch_after
+      end
+
+      it "leaves HEAD a commit ahead of the branch" do
+        commit_change "change"
+
+        assert_equal repo.refs.read_ref("topic"),
+                     resolve_revision("@^")
+      end
+    end
+
+    describe "with concurrent branches" do
+      before do
+        jit_cmd "branch", "fork", "@^"
+      end
+
+      it "advances the branches from a shared parent" do
+        commit_change "A"
+        commit_change "B"
+
+        jit_cmd "checkout", "fork"
+        commit_change "C"
+
+        refute_equal resolve_revision("topic"),
+                     resolve_revision("fork")
+
+        assert_equal resolve_revision("topic~3"),
+                     resolve_revision("fork^")
+      end
+    end
+  end
+end
