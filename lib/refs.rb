009d23c734dfa2402e37ca1a0fe5983925c5ca91
James Coglan <james@jcoglan.com>
2018-04-06 20:13:42 +0100

    Update branch pointers on commit
    To complete the utility of the `branch` and `checkout` commands, we need
    the `commit` command to make branch pointers track commit chains, rather
    than just moving HEAD around. That is, if `.git/HEAD` contains
    
        ref: ref/heads/foo
    
    then `commit` should write the new commit ID to `.git/refs/heads/foo`,
    rather than overwriting `.git/HEAD` and severing the link between HEAD
    and the branch.
    
    To enable this, we make `Refs#update_head` follow symrefs until it
    reaches a ref that either does not exist or contains an object ID. As
    this chain of refs is followed, each ref in turn is locked so that the
    ref does not change between the time it's read and the time its target
    is updated. Once the ultimate target ref is updated, all locks on refs
    leading to it are released.

--- lib/refs.rb
+++ lib/refs.rb
@@ -36,7 +36,7 @@ class Refs
   end
 
   def update_head(oid)
-    update_ref_file(@pathname.join(HEAD), oid)
+    update_symref(@pathname.join(HEAD), oid)
   end
 
   def set_head(revision, oid)
@@ -111,6 +111,28 @@ class Refs
     lockfile = Lockfile.new(path)
 
     lockfile.hold_for_update
+    write_lockfile(lockfile, oid)
+
+  rescue Lockfile::MissingParent
+    FileUtils.mkdir_p(path.dirname)
+    retry
+  end
+
+  def update_symref(path, oid)
+    lockfile = Lockfile.new(path)
+    lockfile.hold_for_update
+
+    ref = read_oid_or_symref(path)
+    return write_lockfile(lockfile, oid) unless ref.is_a?(SymRef)
+
+    begin
+      update_symref(@pathname.join(ref.path), oid)
+    ensure
+      lockfile.rollback
+    end
+  end
+
+  def write_lockfile(lockfile, oid)
     lockfile.write(oid)
     lockfile.write("\n")
     lockfile.commit
