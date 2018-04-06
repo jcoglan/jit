b2dc64b056d40ca7d385b4f64dbbe21a0260373d
James Coglan <james@jcoglan.com>
2018-04-06 21:21:32 +0100

    Set the default branch to "master"
    Now that the `commit` command will update branch pointers rather than
    overwriting `.git/HEAD`, we alter `init` so that a new repository is set
    up to keep commits on a branch by default rather than just using HEAD
    until the user introduces a branch.
    
    Git's default branch is `master`, and we can bring this about simply by
    creating the `.git/refs/heads` directory on init, and placing the
    following content in `.git/HEAD`:
    
        ref: refs/heads/master

--- lib/command/init.rb
+++ lib/command/init.rb
@@ -1,8 +1,12 @@
 require "fileutils"
+
 require_relative "./base"
+require_relative "../refs"
 
 module Command
   class Init < Base
+
+    DEFAULT_BRANCH = "master"
 
     def run
       path = @args.fetch(0, @dir)
@@ -10,7 +14,7 @@ module Command
       root_path = expanded_pathname(path)
       git_path  = root_path.join(".git")
 
-      ["objects", "refs"].each do |dir|
+      ["objects", "refs/heads"].each do |dir|
         begin
           FileUtils.mkdir_p(git_path.join(dir))
         rescue Errno::EACCES => error
@@ -18,6 +22,10 @@ module Command
           exit 1
         end
       end
+
+      refs = Refs.new(git_path)
+      path = File.join("refs", "heads", DEFAULT_BRANCH)
+      refs.update_head("ref: #{ path }")
 
       puts "Initialized empty Jit repository in #{ git_path }"
       exit 0
