8d731db93d1c6d10dfd0375f92edf918d686d9a1
James Coglan <james@jcoglan.com>
2018-05-02 20:40:47 +0100

    Display patches in commit logs
    Having extracted diff-printing logic into `Command::PrintDiff`, and set
    up support for parsing the `--[no-]patch` and `-p`, `-u` and `-s`
    options, we can now add support for the `--patch` option to the `log`
    command. Unlike the `diff` command, `log` should not print patches by
    default.
    
    To display the patch for a commit, we compute the tree diff between its
    parent and itself, which returns a hash of the form
    
        { Pathname => [Database::Entry, Database::Entry] }
    
    For each path in the tree diff, we construct two `PrintDiff::Target`
    objects, one from each `Database::Entry`. These targets are then passed
    to `print_diff` and the existing diff-printing logic takes over.

--- test/command/log_test.rb
+++ test/command/log_test.rb
@@ -122,5 +122,36 @@ describe Command::Log do
         #{ @commits[2].oid } (refs/heads/topic) A
       LOGS
     end
+
+    it "prints a log with patches" do
+      jit_cmd "log", "--pretty=oneline", "--patch"
+
+      assert_stdout <<~LOGS
+        #{ @commits[0].oid } C
+        diff --git a/file.txt b/file.txt
+        index 7371f47..96d80cd 100644
+        --- a/file.txt
+        +++ b/file.txt
+        @@ -1,1 +1,1 @@
+        -B
+        +C
+        #{ @commits[1].oid } B
+        diff --git a/file.txt b/file.txt
+        index 8c7e5a6..7371f47 100644
+        --- a/file.txt
+        +++ b/file.txt
+        @@ -1,1 +1,1 @@
+        -A
+        +B
+        #{ @commits[2].oid } A
+        diff --git a/file.txt b/file.txt
+        new file mode 100644
+        index 0000000..8c7e5a6
+        --- /dev/null
+        +++ b/file.txt
+        @@ -0,0 +1,1 @@
+        +A
+      LOGS
+    end
   end
 end
