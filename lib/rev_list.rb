f2e6a661c5be87cfe454cc9ffe71b0260cf9fbb2
James Coglan <james@jcoglan.com>
2018-05-04 21:34:55 +0100

    Logs with multiple start points
    Until now the `log` command supported following a chain of commits from
    a single starting point. But now suppose we have the following history;
    `parent` links point from right to left, the `master` branch points at
    commit D and `topic` at commit G.
    
        A   B   C   D
        o---o---o---o [master]
             \
              o---o---o [topic]
              E   F   G
    
    If we run `log master topic`, we should see a list of every commit in
    the project where each commit appears exactly once. In particular, we
    should not see a list of all the commits reachable from `master`, and
    then all the commits reachable from `topic` -- this would present a
    confusing chronology and would print commits A and B twice.
    
    Instead, we'd like to display the histories of `master` and `topic` back
    to the point they diverged, followed by their common history. So, an
    order like this would be acceptable:
    
        D, C, G, F, E, B, A
    
    The commits {D, C} must appear in that order because they are directly
    linked by `parent` pointers. Similarly {G, F, E} should be ordered as
    such. However, there is no fixed order between commits on either branch,
    e.g. C is not linked to E via `parent` pointers and so C and E are not
    mutually ordered. All these commits should however appear before B,
    since B is an ancestor of all of them.
    
    `RevList` achieves this using a graph search algorithm. The two starting
    commits D and G are placed in a date-ordered queue. We consume commits
    from this queue, yielding them to the caller if they've not yet been
    visited. We then examine their parents and add them to the queue. A set
    of flags is stored against each commit to mark whether it's been
    processed and its parents followed.
    
    Commits are yielded to the caller as soon as they are found, so we do
    not need to load the entire history into memory before printing any
    output. Instead, output begins as soon as the first commit is loaded.
    
    Assuming users' clocks are roughly accurate, each commit will usually
    have a date that's later than its parent, so prioritising the commit
    queue by date will usually make sure that all descendants of B are
    processed before B itself. So, regardless of the length of the two
    branches, B should be correctly ordered relative to any later commit.

--- lib/rev_list.rb
+++ lib/rev_list.rb
@@ -1,18 +1,58 @@
+require "set"
 require_relative "./revision"
 
 class RevList
-  def initialize(repo, start)
-    @repo  = repo
-    @start = start || Revision::HEAD
+  def initialize(repo, revs)
+    @repo    = repo
+    @commits = {}
+    @flags   = Hash.new { |hash, oid| hash[oid] = Set.new }
+    @queue   = []
+
+    revs.each { |rev| handle_revision(rev) }
+    handle_revision(Revision::HEAD) if @queue.empty?
   end
 
   def each
-    oid = Revision.new(@repo, @start).resolve(Revision::COMMIT)
+    traverse_commits { |commit| yield commit }
+  end
 
-    while oid
-      commit = @repo.database.load(oid)
+  private
+
+  def handle_revision(rev)
+    oid = Revision.new(@repo, rev).resolve(Revision::COMMIT)
+
+    commit = load_commit(oid)
+    enqueue_commit(commit)
+  end
+
+  def enqueue_commit(commit)
+    return unless mark(commit.oid, :seen)
+
+    index = @queue.find_index { |c| c.date < commit.date }
+    @queue.insert(index || @queue.size, commit)
+  end
+
+  def add_parents(commit)
+    return unless mark(commit.oid, :added)
+
+    parent = load_commit(commit.parent)
+    enqueue_commit(parent) if parent
+  end
+
+  def traverse_commits
+    until @queue.empty?
+      commit = @queue.shift
+      add_parents(commit)
       yield commit
-      oid = commit.parent
     end
+  end
+
+  def load_commit(oid)
+    return nil unless oid
+    @commits[oid] ||= @repo.database.load(oid)
+  end
+
+  def mark(oid, flag)
+    @flags[oid].add?(flag)
   end
 end
