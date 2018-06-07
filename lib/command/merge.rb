09f2dc75aadbd3c19c8bbef1ad1d3b145e505bae
James Coglan <james@jcoglan.com>
2018-06-07 10:49:41 +0100

    Basic `merge` command: no conflicts, no unstaged changes
    Having added the infrastructure for finding the common ancestor and
    writing a merge commit over the last few commits, we can now add a basic
    version of the `merge` command. Its working is fairly straightforward
    piecing-together of existing functions.
    
    First we read HEAD and resolve the input argument to a commit ID --
    these are the tips of the merge. Then we find the common ancestor of
    these tips. From this, we work out the tree diff of the merge target
    against the common ancestor, and use a migration to apply that
    difference to the index and workspace. Finally we dump the index to disk
    and write a commit from it, using the two tips as the new commit's
    parents.

---
+++ lib/command/merge.rb
@@ -0,0 +1,34 @@
+require_relative "./base"
+require_relative "./shared/write_commit"
+require_relative "../merge/common_ancestors"
+require_relative "../revision"
+
+module Command
+  class Merge < Base
+
+    include WriteCommit
+
+    def run
+      head_oid  = repo.refs.read_head
+      revision  = Revision.new(repo, @args[0])
+      merge_oid = revision.resolve(Revision::COMMIT)
+
+      common   = ::Merge::CommonAncestors.new(repo.database, head_oid, merge_oid)
+      base_oid = common.find
+
+      repo.index.load_for_update
+
+      tree_diff = repo.database.tree_diff(base_oid, merge_oid)
+      migration = repo.migration(tree_diff)
+      migration.apply_changes
+
+      repo.index.write_updates
+
+      message = @stdin.read
+      write_commit([head_oid, merge_oid], message)
+
+      exit 0
+    end
+
+  end
+end
