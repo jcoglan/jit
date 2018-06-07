cf34a0d52262d245b523393bcda3517a816a5b12
James Coglan <james@jcoglan.com>
2018-06-07 12:09:48 +0100

    Extract `GraphHelper` for testing
    For testing the common ancestor logic, it's useful to have a library of
    functions for easily building arbitrary histories. We've been building
    an ad-hoc set of functions for this in the `Merge::CommonAncestors`
    tests, and now we just extract them into their own module to clean up
    that test suite a little.

---
+++ test/graph_helper.rb
@@ -0,0 +1,35 @@
+require "fileutils"
+require "pathname"
+
+require "database"
+
+module GraphHelper
+  def self.included(suite)
+    suite.before { FileUtils.mkdir_p(db_path) }
+    suite.after  { FileUtils.rm_rf(db_path) }
+  end
+
+  def db_path
+    Pathname.new(File.expand_path("../test-database", __FILE__))
+  end
+
+  def database
+    @database ||= Database.new(db_path)
+  end
+
+  def commit(parents, message)
+    @commits ||= {}
+    @time    ||= Time.now
+
+    parents = parents.map { |oid| @commits[oid] }
+    author  = Database::Author.new("A. U. Thor", "author@example.com", @time)
+    commit  = Database::Commit.new(parents, "0" * 40, author, message)
+
+    database.store(commit)
+    @commits[message] = commit.oid
+  end
+
+  def chain(names)
+    names.each_cons(2) { |parent, message| commit([*parent], message) }
+  end
+end
