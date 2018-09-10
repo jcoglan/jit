3c2466ccfec7cf886f147d4934cd359716c43aa9
James Coglan <james@jcoglan.com>
2018-09-10 13:46:03 +0100

    Remove a single file from the index and workspace
    Here we begin a simple version of the `rm` command. The most basic use
    case we have is to remove files from the index that we no longer want to
    be part of the committed tree. This command takes a list of paths, and
    removes each of them from the index. And, since Git does this, we also
    remove the file from the workspace.
    
    So far this command has no options and does not check for uncommitted
    changes -- this behaviour will be added piece by piece in later commits.

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -55,6 +55,10 @@ class Workspace
     File.open(@pathname.join(path), flags) { |f| f.write(data) }
   end
 
+  def remove(path)
+    File.unlink(@pathname.join(path))
+  end
+
   def apply_migration(migration)
     apply_change_list(migration, :delete)
     migration.rmdirs.sort.reverse_each { |dir| remove_directory(dir) }
