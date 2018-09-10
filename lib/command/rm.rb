e09bb45740d2ae577967470256fecb72534b39a7
James Coglan <james@jcoglan.com>
2018-09-10 14:00:11 +0100

    Don't remove files with unstaged changes
    The `rm` command tries to be safer than the normal rm(1) program, in
    that it tries to prevent you losing data. The easiest way to irrevocably
    lose data is by deleting things from the workspace that have not been
    saved to the database, by being added to the index.
    
    Therefore, we use the `Repository::Inspector` class to check whether the
    file on disk matches the index entry. If not, there are unstaged changes
    and we refuse to remove the file. This check is only done if the file
    exists -- if it's been deleted already, there is no data that would be
    lost by running the `rm` operation.
    
    All arguments are checked for unstaged changes first, and they are only
    removed if it's safe to remove all of them.

--- lib/command/rm.rb
+++ lib/command/rm.rb
@@ -1,10 +1,18 @@
 require_relative "./base"
+require_relative "../repository/inspector"
 
 module Command
   class Rm < Base
 
     def run
       repo.index.load_for_update
+
+      @inspector = Repository::Inspector.new(repo)
+      @unstaged  = []
+
+      @args.each { |path| plan_removal(path) }
+      exit_on_errors
+
       @args.each { |path| remove_file(path) }
       repo.index.write_updates
 
@@ -13,10 +21,31 @@ module Command
 
     private
 
+    def plan_removal(path)
+      entry = repo.index.entry_for_path(path)
+      stat  = repo.workspace.stat_file(path)
+
+      if stat and @inspector.compare_index_to_workspace(entry, stat)
+        @unstaged.push(path)
+      end
+    end
+
     def remove_file(path)
       repo.index.remove(path)
       repo.workspace.remove(path)
       puts "rm '#{ path }'"
+    end
+
+    def exit_on_errors
+      return if @unstaged.empty?
+
+      files_have = (@unstaged.size == 1) ? "file has" : "files have"
+
+      @stderr.puts "error: the following #{ files_have } local modifications:"
+      @unstaged.each { |path| @stderr.puts "    #{ path }" }
+
+      repo.index.release_lock
+      exit 1
     end
 
   end
