48c1779e1ebebeb6fdb23c6ba9405f42b6abd09b
James Coglan <james@jcoglan.com>
2018-09-18 17:57:05 +0100

    Abort partially completed merges
    If a merge gets into a bad state we'd like to be able to escape to the
    state before the merge began. This is now possible as we have
    `Repository#hard_reset`.
    
    Running `merge` to begin with writes the current HEAD ID to ORIG_HEAD.
    During a conflict, running `merge --abort` will recall the commit ID
    from ORIG_HEAD, remove the `.git/MERGE_{HEAD,MSG}` files created by
    `Repository::PendingCommit`, perform a hard reset on the index and
    workspace, and finally update HEAD to point at its pre-merge position.

--- lib/command/merge.rb
+++ lib/command/merge.rb
@@ -11,14 +11,19 @@ module Command
 
     def define_options
       @options[:mode] = :run
+
+      @parser.on("--abort")    { @options[:mode] = :abort    }
       @parser.on("--continue") { @options[:mode] = :continue }
     end
 
     def run
+      handle_abort if @options[:mode] == :abort
       handle_continue if @options[:mode] == :continue
       handle_in_progress_merge if pending_commit.in_progress?
 
       @inputs = ::Merge::Inputs.new(repo, Revision::HEAD, @args[0])
+      repo.refs.update_ref(Refs::ORIG_HEAD, @inputs.left_oid)
+
       handle_merged_ancestor if @inputs.already_merged?
       handle_fast_forward if @inputs.fast_forward?
 
@@ -76,6 +81,19 @@ module Command
       repo.refs.update_head(@inputs.right_oid)
 
       exit 0
+    end
+
+    def handle_abort
+      repo.pending_commit.clear
+
+      repo.index.load_for_update
+      repo.hard_reset(repo.refs.read_head)
+      repo.index.write_updates
+
+      exit 0
+    rescue Repository::PendingCommit::Error => error
+      @stderr.puts "fatal: #{ error.message }"
+      exit 128
     end
 
     def handle_continue
