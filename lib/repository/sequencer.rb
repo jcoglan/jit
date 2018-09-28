03d4431f8f6682a543ac89b4ed7d78cc26b9cacb
James Coglan <james@jcoglan.com>
2018-09-28 14:02:17 +0100

    Abort and quit cherry-pick sequences
    While a cherry-pick sequence is in progress, its state is stored in
    `.git/sequencer`. The `cherry-pick` command supports two options for
    getting out of the cherry-pick state: `--quit` simply deletes the
    pending commit and sequencer state, so the project is left in its
    current state but there is no longer a pending commit. `--abort` also
    does this, but restores the state of the HEAD, index and workspace back
    to the state before the cherry-pick started. This can be used to undo an
    entire cherry-pick sequence if a failure is encountered part-way
    through, letting you treat it as a transaction.
    
    To support these actions, `Sequencer` stores two new files in
    `.git/sequencer`: `abort-safety`, and `head`, both of which initially
    contain the commit ID of the current HEAD. `sequencer/head` keeps its
    value throughout the process -- this is where we find the original HEAD
    value for when `--abort` is used.
    
    `sequencer/abort-safety` is updated after each successfully picked
    commit as a safety mechanism. When `--abort` is used, we want to check
    the state has not been altered by other commands, i.e. it has not
    diverged from the state that the `Sequencer` knows about. If you've made
    other commits the `Sequencer` doesn't know about, it shouldn't reset
    your HEAD as that would throw those commits away. So, before doing this,
    it checks that HEAD matches `abort-safety`, i.e. the current commit is
    one created by `cherry-pick`. If so, we perform a hard reset to get the
    project back to the state of the commit in `sequencer/head`.

--- lib/repository/sequencer.rb
+++ lib/repository/sequencer.rb
@@ -4,16 +4,25 @@ require_relative "../lockfile"
 class Repository
   class Sequencer
 
+    UNSAFE_MESSAGE = "You seem to have moved HEAD. Not rewinding, check your HEAD!"
+
     def initialize(repository)
-      @repo      = repository
-      @pathname  = repository.git_path.join("sequencer")
-      @todo_path = @pathname.join("todo")
-      @todo_file = nil
-      @commands  = []
+      @repo       = repository
+      @pathname   = repository.git_path.join("sequencer")
+      @abort_path = @pathname.join("abort-safety")
+      @head_path  = @pathname.join("head")
+      @todo_path  = @pathname.join("todo")
+      @todo_file  = nil
+      @commands   = []
     end
 
     def start
       Dir.mkdir(@pathname)
+
+      head_oid = @repo.refs.read_head
+      write_file(@head_path, head_oid)
+      write_file(@abort_path, head_oid)
+
       open_todo_file
     end
 
@@ -27,6 +36,7 @@ class Repository
 
     def drop_command
       @commands.shift
+      write_file(@abort_path, @repo.refs.read_head)
     end
 
     def load
@@ -51,11 +61,33 @@ class Repository
       @todo_file.commit
     end
 
+    def abort
+      head_oid = File.read(@head_path).strip
+      expected = File.read(@abort_path).strip
+      actual   = @repo.refs.read_head
+
+      quit
+
+      raise UNSAFE_MESSAGE unless actual == expected
+
+      @repo.hard_reset(head_oid)
+      orig_head = @repo.refs.update_head(head_oid)
+      @repo.refs.update_ref(Refs::ORIG_HEAD, orig_head)
+    end
+
     def quit
       FileUtils.rm_rf(@pathname)
     end
 
     private
+
+    def write_file(path, content)
+      lockfile = Lockfile.new(path)
+      lockfile.hold_for_update
+      lockfile.write(content)
+      lockfile.write("\n")
+      lockfile.commit
+    end
 
     def open_todo_file
       return unless File.directory?(@pathname)
