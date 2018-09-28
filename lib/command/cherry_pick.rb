4fb14a67cfe416506cf0ce38a6d48818515e54d8
James Coglan <james@jcoglan.com>
2018-09-28 11:55:11 +0100

    Interrupt a cherry-pick on conflicts
    Given the changes to `PendingCommit` in the previous commit, we can make
    the `cherry-pick` command persist its state if `Merge::Resolve` results
    in any conflicts in the index. In this event, we store the picked commit
    in `.git/CHERRY_PICK_HEAD` and write the commit's message, plus the list
    of conflicted files in comments, to `.git/MERGE_MSG`, before exiting
    with an error.
    
    When the command is run with `--continue`, then we load the index and
    call a new shared method called `write_cherry_pick_commit`, which
    prompts the user to amend the message before building a new commit from
    this message and the original commit's author.
    
    The `resume_merge` method dispatches to this functionality if a
    cherry-pick is in progress, so the `commit` command will finish the
    cherry-pick in the same way that `cherry-pick --continue` does.

--- lib/command/cherry_pick.rb
+++ lib/command/cherry_pick.rb
@@ -9,7 +9,20 @@ module Command
 
     include WriteCommit
 
+    CONFLICT_NOTES = <<~MSG
+      after resolving the conflicts, mark the corrected paths
+      with 'jit add <paths>' or 'jit rm <paths>'
+      and commit the result with 'jit commit'
+    MSG
+
+    def define_options
+      @options[:mode] = :run
+      @parser.on("--continue") { @options[:mode] = :continue }
+    end
+
     def run
+      handle_continue if @options[:mode] == :continue
+
       revision = Revision.new(repo, @args[0])
       commit   = repo.database.load(revision.resolve)
 
@@ -20,10 +33,14 @@ module Command
 
     private
 
+    def merge_type
+      :cherry_pick
+    end
+
     def pick(commit)
       inputs = pick_merge_inputs(commit)
-
       resolve_merge(inputs)
+      fail_on_conflict(inputs, commit.message) if repo.index.conflict?
 
       picked = Database::Commit.new([inputs.left_oid], write_tree.oid,
                                     commit.author, current_author,
@@ -51,10 +68,35 @@ module Command
       repo.index.write_updates
     end
 
+    def fail_on_conflict(inputs, message)
+      pending_commit.start(inputs.right_oid, merge_type)
+
+      edit_file(pending_commit.message_path) do |editor|
+        editor.puts(message)
+        editor.puts("")
+        editor.note("Conflicts:")
+        repo.index.conflict_paths.each { |name| editor.note("\t#{ name }") }
+        editor.close
+      end
+
+      @stderr.puts "error: could not apply #{ inputs.right_name }"
+      CONFLICT_NOTES.each_line { |line| @stderr.puts "hint: #{ line }" }
+      exit 1
+    end
+
     def finish_commit(commit)
       repo.database.store(commit)
       repo.refs.update_head(commit.oid)
       print_commit(commit)
+    end
+
+    def handle_continue
+      repo.index.load
+      write_cherry_pick_commit
+      exit 0
+    rescue Repository::PendingCommit::Error => error
+      @stderr.puts "fatal: #{ error.message }"
+      exit 128
     end
 
   end
