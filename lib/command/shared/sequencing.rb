f527121f1e540caec7d1d7c54e89b502bb127ee6
James Coglan <james@jcoglan.com>
2018-09-28 16:00:47 +0100

    Extract sequencing code in preparation for `revert`
    We'll shortly add the `revert` command, which works very much like the
    `cherry-pick` command in that it takes a sequence of commits and
    performs a kind of merge with each one. It has the same machinery as
    `cherry-pick` for dealing with failures: `--continue`, `--abort` and
    `--quit` options, and state stored in `.git/sequencer` and
    `.git/REVERT_HEAD` for recovery.
    
    `revert` will differ in how it selects the initial commits, and how it
    constructs each commit after merging, but all the state management
    functionality is the same. Much of this shared behaviour is currently in
    the `CherryPick` class, and by extracting it into `Command::Sequencing`
    we can reuse it with `revert`.

---
+++ lib/command/shared/sequencing.rb
@@ -0,0 +1,116 @@
+require_relative "../../merge/resolve"
+require_relative "../../repository/sequencer"
+
+module Command
+  module Sequencing
+
+    CONFLICT_NOTES = <<~MSG
+      after resolving the conflicts, mark the corrected paths
+      with 'jit add <paths>' or 'jit rm <paths>'
+      and commit the result with 'jit commit'
+    MSG
+
+    def define_options
+      @options[:mode] = :run
+
+      @parser.on("--continue") { @options[:mode] = :continue }
+      @parser.on("--abort")    { @options[:mode] = :abort    }
+      @parser.on("--quit" )    { @options[:mode] = :quit     }
+    end
+
+    def run
+      case @options[:mode]
+      when :continue then handle_continue
+      when :abort    then handle_abort
+      when :quit     then handle_quit
+      end
+
+      sequencer.start
+      store_commit_sequence
+      resume_sequencer
+    end
+
+    private
+
+    def sequencer
+      @sequencer ||= Repository::Sequencer.new(repo)
+    end
+
+    def resolve_merge(inputs)
+      repo.index.load_for_update
+      ::Merge::Resolve.new(repo, inputs).execute
+      repo.index.write_updates
+    end
+
+    def fail_on_conflict(inputs, message)
+      sequencer.dump
+      pending_commit.start(inputs.right_oid, merge_type)
+
+      edit_file(pending_commit.message_path) do |editor|
+        editor.puts(message)
+        editor.puts("")
+        editor.note("Conflicts:")
+        repo.index.conflict_paths.each { |name| editor.note("\t#{ name }") }
+        editor.close
+      end
+
+      @stderr.puts "error: could not apply #{ inputs.right_name }"
+      CONFLICT_NOTES.each_line { |line| @stderr.puts "hint: #{ line }" }
+      exit 1
+    end
+
+    def finish_commit(commit)
+      repo.database.store(commit)
+      repo.refs.update_head(commit.oid)
+      print_commit(commit)
+    end
+
+    def handle_continue
+      repo.index.load
+
+      case pending_commit.merge_type
+      when :cherry_pick then write_cherry_pick_commit
+      end
+
+      sequencer.load
+      sequencer.drop_command
+      resume_sequencer
+
+    rescue Repository::PendingCommit::Error => error
+      @stderr.puts "fatal: #{ error.message }"
+      exit 128
+    end
+
+    def resume_sequencer
+      loop do
+        break unless commit = sequencer.next_command
+        pick(commit)
+        sequencer.drop_command
+      end
+
+      sequencer.quit
+      exit 0
+    end
+
+    def handle_abort
+      pending_commit.clear(merge_type) if pending_commit.in_progress?
+      repo.index.load_for_update
+
+      begin
+        sequencer.abort
+      rescue => error
+        @stderr.puts "warning: #{ error.message }"
+      end
+
+      repo.index.write_updates
+      exit 0
+    end
+
+    def handle_quit
+      pending_commit.clear(merge_type) if pending_commit.in_progress?
+      sequencer.quit
+      exit 0
+    end
+
+  end
+end
