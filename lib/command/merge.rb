b0df3a6924e7dc591d223291e4d55e4a660c1938
James Coglan <james@jcoglan.com>
2018-09-28 11:01:44 +0100

    Support multiple kinds of commit in `PendingCommit`
    The `PendingCommit` class is used to store the inputs to a merge in case
    it has conflicts and needs to be interrupted. The class stores the
    merged commit ID in `.git/MERGE_HEAD` and the message in
    `.git/MERGE_MSG`. When these files exist, running `merge --continue` or
    `commit` will retrieve the merge data from them and create a two-parent
    merge commit.
    
    If a cherry-pick creates conflicts, we want a similar behaviour: the
    inputs should be persisted but the command should exit with an error so
    the user can fix the conflicts and resume the cherry-pick later. But
    when a cherry-pick is resumed, we want to create a normal one-parent
    commit, and reuse the author and message from the picked commit.
    Although a lot of the merge mechanics are the same, the commands differ
    in how they construct the resulting commit.
    
    Therefore we need to be able to tell which type of merge is in progress:
    a true merge, or a cherry-pick. Git differentiates a cherry-pick by
    putting the commit ID in `.git/CHERRY_PICK_HEAD`. We know from the
    existence of this file or `MERGE_HEAD` which operation is in progress.
    
    The `PendingCommit` class's interface needs to change to state which
    type of operation is happening. Its `start`, `merge_oid` and `clear`
    methods now take a `type` argument that defaults to `:merge`. The
    `merge` command explicitly asks for the `:merge` type, whereas `commit`
    checks if any type of merge is in progress and resumes it if so.

--- lib/command/merge.rb
+++ lib/command/merge.rb
@@ -131,7 +131,7 @@ module Command
 
     def handle_continue
       repo.index.load
-      resume_merge
+      resume_merge(:merge)
     rescue Repository::PendingCommit::Error => error
       @stderr.puts "fatal: #{ error.message }"
       exit 128
--- lib/repository/pending_commit.rb
+++ lib/repository/pending_commit.rb
@@ -3,26 +3,42 @@ class Repository
 
     Error = Class.new(StandardError)
 
+    HEAD_FILES = {
+      :merge       => "MERGE_HEAD",
+      :cherry_pick => "CHERRY_PICK_HEAD"
+    }
+
     attr_reader :message_path
 
     def initialize(pathname)
-      @head_path    = pathname.join("MERGE_HEAD")
+      @pathname     = pathname
       @message_path = pathname.join("MERGE_MSG")
     end
 
-    def start(oid)
+    def start(oid, type = :merge)
+      path  = @pathname.join(HEAD_FILES.fetch(type))
       flags = File::WRONLY | File::CREAT | File::EXCL
-      File.open(@head_path, flags) { |f| f.puts(oid) }
+      File.open(path, flags) { |f| f.puts(oid) }
     end
 
     def in_progress?
-      File.file?(@head_path)
+      not merge_type.nil?
     end
 
-    def merge_oid
-      File.read(@head_path).strip
+    def merge_type
+      HEAD_FILES.each do |type, name|
+        path = @pathname.join(name)
+        return type if File.file?(path)
+      end
+
+      nil
+    end
+
+    def merge_oid(type = :merge)
+      head_path = @pathname.join(HEAD_FILES.fetch(type))
+      File.read(head_path).strip
     rescue Errno::ENOENT
-      name = @head_path.basename
+      name = head_path.basename
       raise Error, "There is no merge in progress (#{ name } missing)."
     end
 
@@ -30,11 +46,12 @@ class Repository
       File.read(@message_path)
     end
 
-    def clear
-      File.unlink(@head_path)
+    def clear(type = :merge)
+      head_path = @pathname.join(HEAD_FILES.fetch(type))
+      File.unlink(head_path)
       File.unlink(@message_path)
     rescue Errno::ENOENT
-      name = @head_path.basename
+      name = head_path.basename
       raise Error, "There is no merge to abort (#{ name } missing)."
     end
 
