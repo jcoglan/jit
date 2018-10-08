61fe721c0000679923c77f1861994e3170a46aaa
James Coglan <james@jcoglan.com>
2018-10-08 16:37:03 +0100

    Expand the set of colour codes available
    To support commands that let their colour output be configured, this
    commit adds most of the formatting codes by the name Git uses for them.
    
    This includes a change where, if more than one colour is used in a call
    to `fmt`, the second one becomes a background colour by adding 10 to its
    SGR code.

--- lib/color.rb
+++ lib/color.rb
@@ -1,14 +1,32 @@
 module Color
   SGR_CODES = {
-    "bold"   =>  1,
-    "red"    => 31,
-    "green"  => 32,
-    "yellow" => 33,
-    "cyan"   => 36
+    "normal"  =>  0,
+    "bold"    =>  1,
+    "dim"     =>  2,
+    "italic"  =>  3,
+    "ul"      =>  4,
+    "reverse" =>  7,
+    "strike"  =>  9,
+    "black"   => 30,
+    "red"     => 31,
+    "green"   => 32,
+    "yellow"  => 33,
+    "blue"    => 34,
+    "magenta" => 35,
+    "cyan"    => 36,
+    "white"   => 37
   }
 
   def self.format(style, string)
     codes = [*style].map { |name| SGR_CODES.fetch(name.to_s) }
+    color = false
+
+    codes.each_with_index do |code, i|
+      next unless code >= 30
+      codes[i] += 10 if color
+      color = true
+    end
+
     "\e[#{ codes.join(";") }m#{ string }\e[m"
   end
 end
