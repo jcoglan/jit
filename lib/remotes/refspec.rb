40eeee55578322569d9aef8a5736fc5c84954e55
James Coglan <james@jcoglan.com>
2018-10-11 11:04:20 +0100

    Method for expanding refspecs
    When running the `fetch` command, we will have a couple of pieces of
    input to decide what to fetch from the remote. First, there will be a
    set of refspecs that we have configured for the remote, for example:
    
        [remote "github"]
            fetch = +refs/heads/*:refs/remotes/github/*
    
    This means we should copy all the refs from `refs/heads` in the remote
    repo to `refs/remotes/github` in our own. The second piece of input is
    the list of refs from the remote, for example:
    
        refs/heads/maint
        refs/heads/master
        refs/heads/todo
    
    The method `Refspec.expand` takes these two values and produces the
    following structure, showing which of our local refs we should update
    and where their values should come from.
    
        {
          "refs/remotes/github/maint"  => ["refs/heads/maint",  true],
          "refs/remotes/github/master" => ["refs/heads/master", true],
          "refs/remotes/github/todo"   => ["refs/heads/todo",   true]
        }
    
    The boolean in each block shows whether updates to our local ref should
    be forced; the `+` in the refspec indicates they should be.
    
    The `*` in the refspec patterns is used to match as much of the inputs
    as possible. If we instead have this refspec:
    
        [remote "github"]
            fetch = +refs/heads/ma*:refs/remotes/github/*
    
    Then only the `maint` and `master` branches would match, and the
    remainder of their names after `ma` would become our local name for
    them.
    
        {
          "refs/remotes/github/int"  => ["refs/heads/maint",  true],
          "refs/remotes/github/ster" => ["refs/heads/master", true]
        }
    
    If a refspec does not contain a `*`, then it is not matched against the
    list of refs but returned verbatim.

--- lib/remotes/refspec.rb
+++ lib/remotes/refspec.rb
@@ -1,6 +1,36 @@
 class Remotes
 
+  REFSPEC_FORMAT = /^(\+?)([^:]+):([^:]+)$/
+
   Refspec = Struct.new(:source, :target, :forced) do
+    def self.parse(spec)
+      match = REFSPEC_FORMAT.match(spec)
+      Refspec.new(match[2], match[3], match[1] == "+")
+    end
+
+    def self.expand(specs, refs)
+      specs = specs.map { |spec| parse(spec) }
+
+      specs.reduce({}) do |mappings, spec|
+        mappings.merge(spec.match_refs(refs))
+      end
+    end
+
+    def match_refs(refs)
+      return { target => [source, forced] } unless source.to_s.include?("*")
+
+      pattern  = /^#{ source.sub("*", "(.*)") }$/
+      mappings = {}
+
+      refs.each do |ref|
+        next unless match = pattern.match(ref)
+        dst = match[1] ? target.sub("*", match[1]) : target
+        mappings[dst] = [ref, forced]
+      end
+
+      mappings
+    end
+
     def to_s
       spec = forced ? "+" : ""
       spec + [source, target].join(":")
