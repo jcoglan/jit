3a9fb7a3c894a325d1def0a7f755b34699d90c32
James Coglan <james@jcoglan.com>
2018-10-25 09:05:04 +0100

    Introduce the `rev-list` command
    The `log`, `cherry-pick` and `revert` commands use the internal
    `RevList` class to query sections of the commit history. To generate
    sets of objects to send over the network in the `fetch` command, we're
    going to reuse this class and expand its capabilities even further.
    
    To aid in checking that it's selecting the right objects, the `rev-list`
    command will be a replica of that in Git, that passes its input to
    `RevList` and prints the objects it finds. This is like a low-level
    version of `log` that just prints object IDs, not any higher-level
    information about each object.
    
    Initially the command only supports the `:walk` option via its
    `--do-walk` and `--no-walk` switches.

---
+++ lib/command/rev_list.rb
@@ -0,0 +1,22 @@
+require_relative "./base"
+require_relative "../rev_list"
+
+module Command
+  class RevList < Base
+
+    def define_options
+      @options[:walk] = true
+
+      @parser.on("--do-walk") { @options[:walk] = true  }
+      @parser.on("--no-walk") { @options[:walk] = false }
+    end
+
+    def run
+      rev_list = ::RevList.new(repo, @args, @options)
+      rev_list.each { |object| puts object.oid }
+
+      exit 0
+    end
+
+  end
+end
