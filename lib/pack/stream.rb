309d66523977cf0a84fd15b5a4f29c8b18a73eb4
James Coglan <james@jcoglan.com>
2018-10-30 15:09:22 +0000

    Support seeking backwards in an incoming pack stream
    When reading objects from a pack, there's no indication of how long the
    zlib-compressed buffer for each object is -- the record header gives the
    size before compression, not after. Since we don't know where the object
    ends, the safest thing to do is feed input into zlib one byte at a time
    and stop when zlib says the stream is finished.
    
        def read_zlib_stream
          stream = Zlib::Inflate.new
          string = ""
    
          until stream.finished?
            string.concat(stream.inflate(@input.read(1)))
          end
    
          string
        end
    
    However, drip-feeding data into zlib like this is really slow, as a our
    program spends most of its time in a Ruby loop calling `read` and
    `finished?`. By reading in chunks we spend more time in zlib on each
    iteration, but we might read past the end of the object. We need to
    relocate the read cursor to the exact end of the object so we can begin
    reading the next one. A method for doing that using the Ruby `IO`
    interface might look like this:
    
        def read_zlib_stream
          stream = Zlib::Inflate.new
          string = ""
          total  = 0
    
          until stream.finished?
            data   = @input.read_nonblock(256)
            total += data.bytesize
    
            string.concat(stream.inflate(data))
          end
          @input.seek(stream.total_in - total, IO::SEEK_CUR)
    
          string
        end
    
    However, streaming `IO` sources don't support `seek` -- once you've read
    some data, the `IO` object forgets about it. We can recover this kind of
    behaviour by implementing it on `Pack::Stream` which will wrap the
    underlying `IO` stream.
    
    By using the `capture` method, the caller can make `Stream` store up any
    data that's read while some block is executed. `seek` can then use this
    captured data to put some of it back in a buffer. The `read` and
    `read_nonblock` methods pull from this buffer before fetching more data
    from the underlying `IO`.
    
    If capturing is enabled, then data is not passed into the SHA-1 check
    until the capture session ends, as if we read past the end of an object
    we might be reading the SHA-1 at the end of the pack. So, we only update
    our digest once we know how much data actually contributed to the
    current object.

--- lib/pack/stream.rb
+++ lib/pack/stream.rb
@@ -6,19 +6,37 @@ module Pack
     attr_reader :digest, :offset
 
     def initialize(input)
-      @input  = input
-      @digest = Digest::SHA1.new
-      @offset = 0
+      @input   = input
+      @digest  = Digest::SHA1.new
+      @offset  = 0
+      @buffer  = new_byte_string
+      @capture = nil
+    end
+
+    def capture
+      @capture = new_byte_string
+      result   = [yield, @capture]
+
+      @digest.update(@capture)
+      @capture = nil
+
+      result
     end
 
     def verify_checksum
-      unless @input.read(20) == @digest.digest
+      unless read_buffered(20) == @digest.digest
         raise InvalidPack, "Checksum does not match value read from pack"
       end
     end
 
     def read(size)
-      data = @input.read(size)
+      data = read_buffered(size)
+      update_state(data)
+      data
+    end
+
+    def read_nonblock(size)
+      data = read_buffered(size, false)
       update_state(data)
       data
     end
@@ -27,11 +45,35 @@ module Pack
       read(1).bytes.first
     end
 
+    def seek(amount, whence = IO::SEEK_SET)
+      return unless amount < 0
+
+      data = @capture.slice!(amount .. -1)
+      @buffer.prepend(data)
+      @offset += amount
+    end
+
     private
 
+    def new_byte_string
+      String.new("", :encoding => Encoding::ASCII_8BIT)
+    end
+
+    def read_buffered(size, block = true)
+      from_buf = @buffer.slice!(0, size)
+      needed   = size - from_buf.bytesize
+      from_io  = block ? @input.read(needed) : @input.read_nonblock(needed)
+
+      from_buf.concat(from_io.to_s)
+
+    rescue EOFError, Errno::EWOULDBLOCK
+      from_buf
+    end
+
     def update_state(data)
-      @digest.update(data)
+      @digest.update(data) unless @capture
       @offset += data.bytesize
+      @capture&.concat(data)
     end
 
   end
