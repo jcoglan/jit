51213266279e0bad31cddfe672f689e6ac326534
James Coglan <james@jcoglan.com>
2018-11-16 15:31:41 +0000

    Display progress of pack transfers
    Here we introduce a `Progress` class that can display an updating
    read-out of the progress of an operation, counting how many items have
    been processed and the percentage completion if an anticipated total is
    provided. It can also display how much data has been transferred if
    `tick` is called with a numeric argument.
    
    We use this in a few places to make the pack transfer process less
    opaque. In `Pack::Writer`, we show the objects being counted and then
    written to the pack. In `ReceiveObjects`, we show the objects being
    unpacked on the other end of the connection.

--- lib/command/shared/receive_objects.rb
+++ lib/command/shared/receive_objects.rb
@@ -1,18 +1,24 @@
 require_relative "../../pack"
+require_relative "../../progress"
 
 module Command
   module ReceiveObjects
 
     def recv_packed_objects(prefix = "")
-      stream = Pack::Stream.new(@conn.input, prefix)
-      reader = Pack::Reader.new(stream)
+      stream   = Pack::Stream.new(@conn.input, prefix)
+      reader   = Pack::Reader.new(stream)
+      progress = Progress.new(@stderr) unless @conn.input == STDIN
 
       reader.read_header
+      progress&.start("Unpacking objects", reader.count)
 
       reader.count.times do
         record, _ = stream.capture { reader.read_record }
         repo.database.store(record)
+        progress&.tick(stream.offset)
       end
+      progress&.stop
+
       stream.verify_checksum
     end
 
