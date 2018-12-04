731428521b5f237146a4f00c703a424b19b711e1
James Coglan <james@jcoglan.com>
2018-12-04 16:35:27 +0000

    Serialisation for delta copy and insert operations
    The `XDelta` module returns arrays of `Delta::Copy` and `Delta::Insert`
    objects. To write these into a pack, these need to be serialised, and
    then need to be decoded on the other end.
    
    This commit implements `to_s` and `parse` on these classes to support
    this requirement. Copy instructions are serialised using the
    `PackedInt56LE` format with the high bit set, and insert instructions
    are just a length header (up to 127 or 0x7f) followed by the literal
    data.

--- lib/pack/delta.rb
+++ lib/pack/delta.rb
@@ -1,8 +1,33 @@
+require_relative "./numbers"
+
 module Pack
   class Delta
 
-    Copy   = Struct.new(:offset, :size)
-    Insert = Struct.new(:data)
+    Copy = Struct.new(:offset, :size) do
+      def self.parse(input, byte)
+        value  = Numbers::PackedInt56LE.read(input, byte)
+        offset = value & 0xffffffff
+        size   = value >> 32
+
+        Copy.new(offset, size)
+      end
+
+      def to_s
+        bytes = Numbers::PackedInt56LE.write((size << 32) | offset)
+        bytes[0] |= 0x80
+        bytes.pack("C*")
+      end
+    end
+
+    Insert = Struct.new(:data) do
+      def self.parse(input, byte)
+        Insert.new(input.read(byte))
+      end
+
+      def to_s
+        [data.bytesize, data].pack("Ca*")
+      end
+    end
 
   end
 end
