859c9af7cdea561c29f6231d130e7bdc2b51a454
James Coglan <james@jcoglan.com>
2018-12-13 15:40:37 +0000

    Discourage long delta chains that provide little benefit
    When compressing a large set of objects, long chains of deltas can form:
    structures where a delta record has another delta record as its base,
    forming a chain. When a pack is used as primary on-disk storage for
    objects, every link in a delta chain adds to the cost of reconstructing
    an object, as the base object must be located and expanded first.
    
    So we should only pay that cost if it results in substantial size
    savings. Empirically, it seems that objects tend to compress either
    extremely well (> 90% size reduction) or very poorly (< 10%). By setting
    a minimum size reduction threshold we can avoid creating delta chains
    that don't provide much size reduction.
    
    The conditions introduced here reject a delta unless it reduces an
    object's size by at least half. If the object has already been assigned
    a delta, this threshold is adjusted by the relative depth of the
    existing and proposed delta chains, to favour shorter ones.

--- lib/pack/compressor.rb
+++ lib/pack/compressor.rb
@@ -45,13 +45,38 @@ module Pack
       return unless source.type == target.type
       return unless source.depth < MAX_DEPTH
 
+      max_size = max_size_heuristic(source, target)
+      return unless compatible_sizes?(source, target, max_size)
+
       delta = Delta.new(source, target)
       size  = target.entry.packed_size
 
-      return if delta.size > size
+      return if delta.size > max_size
       return if delta.size == size and delta.base.depth + 1 >= target.depth
 
       target.entry.assign_delta(delta)
+    end
+
+    def max_size_heuristic(source, target)
+      if target.delta
+        max_size  = target.delta.size
+        ref_depth = target.depth
+      else
+        max_size  = target.size / 2 - 20
+        ref_depth = 1
+      end
+
+      max_size * (MAX_DEPTH - source.depth) / (MAX_DEPTH + 1 - ref_depth)
+    end
+
+    def compatible_sizes?(source, target, max_size)
+      size_diff = [target.size - source.size, 0].max
+
+      return false if max_size == 0
+      return false if size_diff >= max_size
+      return false if target.size < source.size / 32
+
+      true
     end
 
   end
